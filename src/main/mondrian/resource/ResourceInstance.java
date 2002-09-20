e005d7545e842f47ccfab8581bb67b976c9a38b9
Julian Hyde <julianhyde@gmail.com>
2002-09-19 16:26:07 -0800

    MONDRIAN: Oops! (Forgot a couple of files.)
    [git-p4: depot-paths = "//open/mondrian/": change = 159]

---
+++ src/main/mondrian/resource/ResourceInstance.java
@@ -0,0 +1,25 @@
+/*
+// $Id$
+// This software is subject to the terms of the Common Public License
+// Agreement, available at the following URL:
+// http://www.opensource.org/licenses/cpl.html.
+// (C) Copyright 2002 Kana Software, Inc. and others.
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+//
+// jhyde, 19 September, 2002
+*/
+package mondrian.resource;
+
+import java.util.ResourceBundle;
+
+/**
+ * A <code>ResourceInstance</code> is an occurrence of a {@link
+ * ResourceDefinition} with a set of arguments. It can later be formatted to a
+ * specific locale.
+ */
+public interface ResourceInstance {
+	public String toString();
+}
+
+// End ResourceInstance.java
