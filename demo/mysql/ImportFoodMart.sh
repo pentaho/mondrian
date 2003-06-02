0fec48bc4c2a912761e7deb9bb683a5ed196f6e8
Julian Hyde <julianhyde@gmail.com>
2003-06-02 00:18:48 -0800

    MONDRIAN: Remove obsolete MySQL data files.
    [git-p4: depot-paths = "//open/mondrian/": change = 577]

--- demo/mysql/ImportFoodMart.sh
+++
@@ -1,9 +0,0 @@
-
-mysqladmin -u root -f drop foodmart
-mysqladmin -u root -f create foodmart
-
-cat ../oracle/FoodMartTables.sql | ./fixtables | mysql -u root foodmart
-cat ../oracle/FoodMartData.sql | ./fixdata | mysql -u root foodmart
-cat ../oracle/FoodMartIndexes.sql | ./fixindexes | mysql -u root foodmart
-
-
