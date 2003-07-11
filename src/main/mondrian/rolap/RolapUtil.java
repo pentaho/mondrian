198da83af1df99b26a29b50055745938002f0904
robinchiedo <a@b>
2003-07-11 08:50:30 -0800

    Added RolapConnectionPool class to RolapConnection, implements singleton connection pool. Moved getPoolingDataSource from RolapUtil to this class.
    [git-p4: depot-paths = "//open/mondrian/": change = 651]

--- src/main/mondrian/rolap/RolapUtil.java
+++ src/main/mondrian/rolap/RolapUtil.java
@@ -314,58 +314,6 @@ public class RolapUtil {
 		return querySemaphore;
 	}
 
-    /**
-	 * Sets up a pooling data source for connection pooling.
-	 * This can be used if the application server does not have a pooling
-	 * dataSource already configured.
-	 * This takes a normal jdbc connection string, and requires a jdbc
-	 * driver to be loaded, and then uses a
-     * {@link DriverManagerConnectionFactory} to create connections to the
-     * database.
-	 * An alternative method of configuring a pooling driver is to use an external
-	 * configuration file. See the the Apache jakarta-commons commons-pool
-	 * documentation.
-	 * @param connectionFactory Creates connections from an underlying
-     *   JDBC connect string or DataSource
-	 * @return a pooling DataSource object
-	 */
-	static DataSource getPoolingDataSource(ConnectionFactory connectionFactory) {
-
-		// use GenericObjectPool, which provides for resource limits
-		ObjectPool connectionPool = new GenericObjectPool(
-			null, // PoolableObjectFactory, can be null
-			50, // max active
-			GenericObjectPool.WHEN_EXHAUSTED_FAIL, // action when exhausted
-			3000, // max wait (milli seconds)
-			10, // max idle
-			false, // test on borrow
-			false, // test on return
-			60000, // time between eviction runs (millis)
-			5, // number to test on eviction run
-			30000, // min evictable idle time (millis)
-			true // test while idle
-			);
-
-		// create a PoolableConnectionFactory
-		AbandonedConfig abandonedConfig = new AbandonedConfig();
-		abandonedConfig.setRemoveAbandoned(true); // flag to remove abandoned connections from pool
-		abandonedConfig.setRemoveAbandonedTimeout(300); // timeout (seconds) before removing abandoned connections
-		abandonedConfig.setLogAbandoned(true); // Flag to log stack traces for application code which abandoned a Statement or Connection
-        PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(
-            connectionFactory, // the connection factory
-            connectionPool, // the object pool
-            null, // statement pool factory for pooling prepared statements, or null for no pooling
-            null, // validation query (must return at least 1 row e.g. Oracle: select count(*) from dual) to test connection, can be null
-            false, // default "read only" setting for borrowed connections
-            true, // default "auto commit" setting for returned connections
-            abandonedConfig // AbandonedConfig object configures how to handle abandoned connections
-            );
-        // "poolableConnectionFactory" has registered itself with
-        // "connectionPool", somehow, so we don't need the value any more.
-        Util.discard(poolableConnectionFactory);
-		// create pooling datasource
-		return new PoolingDataSource(connectionPool);
-	}
 
     static class Semaphore {
 		private int count;
