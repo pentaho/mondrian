d293234ee13d85f04830eb72a80a67c8043dacfd
Andreas Voss <tonbeller(@)ignore.a-voss.de>
2005-11-28 01:22:25 -0800

    MONDRIAN: needed if the locale does not contain country info
    [git-p4: depot-paths = "//open/mondrian/": change = 4497]

---
+++ src/main/mondrian/resource/MondrianResource_de.java
@@ -0,0 +1,23 @@
+// This class is generated. Do NOT modify it, or
+// add it to source control.
+
+package mondrian.resource;
+import java.io.IOException;
+import java.util.Locale;
+import org.eigenbase.resgen.*;
+
+/**
+ * This class was generated
+ * by class org.eigenbase.resgen.ResourceGen
+ * from C:/dev/mondrian.perforce/src/main/mondrian/resource/MondrianResource.xml
+ * on Mon Nov 28 10:08:57 CET 2005.
+ * It contains a list of messages, and methods to
+ * retrieve and format those messages.
+ */
+
+public class MondrianResource_de extends MondrianResource {
+    public MondrianResource_de() throws IOException {
+    }
+}
+
+// End MondrianResource_de_DE.java
