a14783e045e0bcdb5b1617f336d71ffd2ee8d23e
Julian Hyde <julianhyde@gmail.com>
2005-12-22 17:54:32 -0800

    MONDRIAN: Oops!
    [git-p4: depot-paths = "//open/mondrian/": change = 4807]

---
+++ src/main/mondrian/spi/CatalogLocator.java
@@ -0,0 +1,28 @@
+/*
+// $Id$
+// This software is subject to the terms of the Common Public License
+// Agreement, available at the following URL:
+// http://www.opensource.org/licenses/cpl.html.
+// (C) Copyright 2005-2005 Julian Hyde
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+*/
+package mondrian.spi;
+
+/**
+ * Abstract layer for locating catalog schema content.
+ *
+ * @author Gang Chen
+ * @since December, 2005
+ * @version $Id$
+ */
+public interface CatalogLocator {
+
+    /**
+     * @return URL complied string representation.
+     */
+    String locate(String catalogPath);
+
+}
+
+// End CatalogLocator.java
