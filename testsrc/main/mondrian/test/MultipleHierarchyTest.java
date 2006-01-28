36385f27e90c0aa6ad5ec8efae59ef0bea45df0c
Julian Hyde <julianhyde@gmail.com>
2006-01-27 16:24:43 -0800

    MONDRIAN: Test PeriodsToDate against both monthly and weekly hierarchies.
    [git-p4: depot-paths = "//open/mondrian/": change = 5242]

--- testsrc/main/mondrian/test/MultipleHierarchyTest.java
+++ testsrc/main/mondrian/test/MultipleHierarchyTest.java
@@ -73,6 +73,48 @@ public class MultipleHierarchyTest extends FoodMartTestCase {
             "where ([Gender].[M], [Time.Weekly].[1997], [Time].[1997])"}),
             "Tuple contains more than one member of dimension '[Time]'.");
     }
+
+    public void testCalcMember() {
+        assertQueryReturns(
+                "with member [Measures].[Sales to Date] as \n" +
+                " ' Sum(PeriodsToDate([Time].[Year], [Time].CurrentMember), [Measures].[Unit Sales])'\n" +
+                "select {[Measures].[Sales to Date]} on columns,\n" +
+                " {[Time].[1997].[Q2].[4]," +
+                "  [Time].[1997].[Q2].[5]} on rows\n" +
+                "from [Sales]",
+                // msas give 86740, 107551
+                fold(new String[] {
+                    "Axis #0:",
+                    "{}",
+                    "Axis #1:",
+                    "{[Measures].[Sales to Date]}",
+                    "Axis #2:",
+                    "{[Time].[1997].[Q2].[4]}",
+                    "{[Time].[1997].[Q2].[5]}",
+                    "Row #0: 86,470",
+                    "Row #1: 107,551",
+                    ""}));
+
+        assertQueryReturns(
+                "with member [Measures].[Sales to Date] as \n" +
+                " ' Sum(PeriodsToDate([Time.Weekly].[Year], [Time].CurrentMember), [Measures].[Unit Sales])'\n" +
+                "select {[Measures].[Sales to Date]} on columns,\n" +
+                " {[Time.Weekly].[1997].[14] : [Time.Weekly].[1997].[16]} on rows\n" +
+                "from [Sales]",
+                fold(new String[] {
+                    "Axis #0:",
+                    "{}",
+                    "Axis #1:",
+                    "{[Measures].[Sales to Date]}",
+                    "Axis #2:",
+                    "{[Time.Weekly].[All Time.Weeklys].[1997].[14]}",
+                    "{[Time.Weekly].[All Time.Weeklys].[1997].[15]}",
+                    "{[Time.Weekly].[All Time.Weeklys].[1997].[16]}",
+                    "Row #0: 81,670",
+                    "Row #1: 86,300",
+                    "Row #2: 90,139",
+                    ""}));
+    }
 }
 
 // End MultipleHierarchyTest.java
