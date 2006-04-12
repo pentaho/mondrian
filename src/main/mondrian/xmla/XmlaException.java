8223daee3d6e38421d2cda003266824e604ace85
Richard Emberson <remberson(@)ignore.edgedynamics.com>
2006-04-12 04:45:14 -0800

    MONDRIAN        XMLA Exception class used to generate SOAP fault body.
    [git-p4: depot-paths = "//open/mondrian/": change = 6212]

--- src/main/mondrian/xmla/XmlaException.java
+++ src/main/mondrian/xmla/XmlaException.java
@@ -3,60 +3,82 @@
 // This software is subject to the terms of the Common Public License
 // Agreement, available at the following URL:
 // http://www.opensource.org/licenses/cpl.html.
-// Copyright (C) 2006-2006 Julian Hyde
+// Copyright (C) 2004-2005 TONBELLER AG
 // All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 */
 package mondrian.xmla;
 
-/**
- * ...
- *
- * @author jhyde
- * @version $Id$
- * @since Apr 12, 2006
- */
-public class XmlaException extends RuntimeException {
-    private final String code;
-    private final String faultString;
+import mondrian.olap.MondrianException;
 
-    public XmlaException(String message,Throwable cause) {
-        this(null, null, message, cause);
+/** 
+ * An exception thrown while processing an XMLA request. The faultcode
+ * corresponds to the SOAP Fault faultcode and the faultstring
+ * to the SOAP Fault faultstring.
+ * 
+ * @author <a>Richard M. Emberson</a>
+ * @version $Id$
+ */
+public class XmlaException extends MondrianException {
+
+    public static String formatFaultCode(XmlaException xex) {
+        return formatFaultCode(xex.getFaultCode(), xex.getCode());
+    }
+    public static String formatFaultCode(String faultCode, String code) {
+        return formatFaultCode(XmlaConstants.SOAP_PREFIX, 
+                faultCode, code);
+    }
+    public static String formatFaultCode(String nsPrefix, 
+                String faultCode, String code) {
+        return nsPrefix + 
+            ':' + 
+            faultCode +
+            '.' + 
+            code;
+    }
+    public static String formatDetail(String msg) {
+        return XmlaConstants.FAULT_FS_PREFIX + msg;
     }
 
-    public XmlaException(String faultString, String code, String message,Throwable cause) {
-        super(message, cause);
+    public static Throwable getRootCause(Throwable throwable) {
+        Throwable t = throwable;
+        while (t.getCause() != null) {
+            t = t.getCause();
+        }
+        return t;
+    }
+
+    private final String faultCode; 
+    private final String code; 
+    private final String faultString; 
+
+    public XmlaException(
+            String faultCode, 
+            String code, 
+            String faultString,
+            Throwable cause) {
+        super(faultString, cause);
+        this.faultCode = faultCode;
         this.code = code;
         this.faultString = faultString;
     }
 
+    public String getFaultCode() {
+        return faultCode;
+    }
     public String getCode() {
         return code;
     }
-
     public String getFaultString() {
         return faultString;
     }
-
-    public static String formatFaultCode(XmlaException xex) {
-        return xex.faultString;
-    }
-
-    public static String formatDetail(Object detail) {
-        throw new UnsupportedOperationException();
-    }
-
-    public Object getDetail() {
-        throw new UnsupportedOperationException();
-    }
-
-    public static Throwable getRootCause(Throwable t) {
-        throw new UnsupportedOperationException();
-    }
-
-    public static String formatFaultCode(String serverFaultFc, String code) {
-        throw new UnsupportedOperationException();
+    public String getDetail() {
+        Throwable t = getCause();
+        t = getRootCause(t);
+        String detail = t.getMessage();
+        return (detail != null)
+            ? detail
+            : t.getClass().getName();
     }
 }
 
-// End XmlaException.java
