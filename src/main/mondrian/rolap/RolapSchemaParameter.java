6bd9838ee2cd62ff0ab3580033594b69549db0e6
Julian Hyde <julianhyde@gmail.com>
2006-08-03 01:13:45 -0800

    MONDRIAN: Oops, forgot a file.
    [git-p4: depot-paths = "//open/mondrian/": change = 7324]

---
+++ src/main/mondrian/rolap/RolapSchemaParameter.java
@@ -0,0 +1,127 @@
+/*
+// $Id$
+// This software is subject to the terms of the Common Public License
+// Agreement, available at the following URL:
+// http://www.opensource.org/licenses/cpl.html.
+// Copyright (C) 2006-2006 Julian Hyde
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+*/
+package mondrian.rolap;
+
+import mondrian.olap.*;
+import mondrian.olap.type.Type;
+import mondrian.calc.ParameterCompilable;
+import mondrian.calc.Calc;
+import mondrian.calc.ExpCompiler;
+import mondrian.calc.impl.GenericCalc;
+import mondrian.resource.MondrianResource;
+
+/**
+ * Parameter which is defined in a schema.
+ *
+ * @author jhyde
+ * @version $Id$
+ * @since Jul 20, 2006
+ */
+public class RolapSchemaParameter implements Parameter, ParameterCompilable {
+    private final RolapSchema schema;
+    private final String name;
+    private String description;
+    private String defaultExpString;
+    private Type type;
+    private final boolean modifiable;
+    private Object value;
+    private Object cachedDefaultValue;
+
+    RolapSchemaParameter(
+        RolapSchema schema,
+        String name,
+        String defaultExpString,
+        String description,
+        Type type,
+        boolean modifiable)
+    {
+        assert defaultExpString != null;
+        assert name != null;
+        assert schema != null;
+        assert type != null;
+        this.schema = schema;
+        this.name = name;
+        this.defaultExpString = defaultExpString;
+        this.description = description;
+        this.type = type;
+        this.modifiable = modifiable;
+        schema.parameterList.add(this);
+    }
+
+    RolapSchema getSchema() {
+        return schema;
+    }
+
+    public boolean isModifiable() {
+        return modifiable;
+    }
+
+    public Scope getScope() {
+        return Scope.Schema;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public Exp getDefaultExp() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        if (!modifiable) {
+            throw MondrianResource.instance().ParameterIsNotModifiable.ex(
+                getName(), getScope().name);
+        }
+        this.value = value;
+    }
+
+    public Calc compile(ExpCompiler compiler) {
+        // Parse and compile the expression for the default value.
+        Exp defaultExp = compiler.getValidator()
+            .getQuery()
+            .getConnection()
+            .parseExpression(defaultExpString);
+        defaultExp = compiler.getValidator().validate(defaultExp, true);
+        final Calc defaultCalc = defaultExp.accept(compiler);
+
+        // Generate a program which looks at the assigned value first,
+        // and if it is not set, returns the default expression.
+        return new GenericCalc(defaultExp) {
+            public Calc[] getCalcs() {
+                return new Calc[] {defaultCalc};
+            }
+
+            public Object evaluate(Evaluator evaluator) {
+                if (value != null) {
+                    return value;
+                }
+                if (cachedDefaultValue == null) {
+                    cachedDefaultValue = defaultCalc.evaluate(evaluator);
+                }
+                return cachedDefaultValue;
+            }
+        };
+    }
+}
+
+// End RolapSchemaParameter.java
