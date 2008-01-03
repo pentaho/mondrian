e7a399ebf2424bb4ae09740a7563451fa843f727
Khanh Vu <kvu@lucidera.com>
2008-01-03 09:44:12 -0800

    QA-MONDRIAN: ports tests for LER-5022 and related issues
    [git-p4: depot-paths = "//open/mondrian/": change = 10386]

--- testsrc/main/mondrian/test/clearview/MemHungryTest.java
+++ testsrc/main/mondrian/test/clearview/MemHungryTest.java
@@ -1,3 +1,13 @@
+/*
+// $Id$
+// This software is subject to the terms of the Common Public License
+// Agreement, available at the following URL:
+// http://www.opensource.org/licenses/cpl.html.
+// Copyright (C) 2007 Julian Hyde and others
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+//
+*/
 package mondrian.test.clearview;
 
 import junit.framework.TestSuite;
@@ -5,7 +15,12 @@ import junit.framework.Test;
 import mondrian.test.DiffRepository;
 
 /**
- * TODO:  Even the humblest classes deserve comments.
+ * <code>MemHungryTest</code> is a test suite which tests
+ * complex queries against the FoodMart database. MDX queries and their
+ * expected results are maintained separately in MemHungryTest.ref.xml file.
+ * If you would prefer to see them as inlined Java string literals, run
+ * ant target "generateDiffRepositoryJUnit" and then use
+ * file MemHungryTestJUnit.java which will be generated in this directory.
  *
  * @author Khanh Vu
  * @version $Id$
