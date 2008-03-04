200c815334437140d1dace928d0c8c126c01d8c1
Julian Hyde <julianhyde@gmail.com>
2008-03-03 22:06:06 -0800

    MONDRIAN: Oops!
    [git-p4: depot-paths = "//open/mondrian/": change = 10643]

--- testsrc/main/mondrian/test/DrillThroughTest.java
+++ testsrc/main/mondrian/test/DrillThroughTest.java
@@ -672,7 +672,7 @@ public class DrillThroughTest extends FoodMartTestCase {
             final String columnName = resultSet.getMetaData().getColumnName(5);
             assertTrue(
                 columnName,
-                columnName.startsWith("Education Level but with a very long"));
+                columnName.startsWith("Education Level but with a"));
             int n = 0;
             while (resultSet.next()) {
                 ++n;
