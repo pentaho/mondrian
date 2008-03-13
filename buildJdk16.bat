55a8fd039025c24cf1b05507c2d272d39fdf094b
Julian Hyde <julianhyde@gmail.com>
2008-03-13 14:01:14 -0800

    MONDRIAN: Fix buildJdk16.bat; now 'ant jar' works on windows.
    [git-p4: depot-paths = "//open/mondrian/": change = 10708]

--- buildJdk16.bat
+++ buildJdk16.bat
@@ -1,11 +1,16 @@
-# $Id$
-# Called recursively from 'ant release' to build the files which can only be
-# built under JDK 1.6.
+@echo off
+rem $Id$
+rem Called recursively from 'ant release' to build the files which can only be
+rem built under JDK 1.6.
 
-# Change the following line to point to your JDK 1.6 home.
+rem Change the following line to point to your JDK 1.6 home.
 set JAVA_HOME=C:\jdk1.6.0_01
-set PATH=%JAVA_HOME%\bin;%PATH%
-ant compile.java
 
-# End buildJdk16.bat
+rem Change the following line to point to your ant home.
+set ANT_HOME=C:\open\thirdparty\ant
+
+set PATH=%JAVA_HOME%\bin;%PATH%
+%ANT_HOME%\bin\ant compile.java
+
+rem End buildJdk16.bat
 
