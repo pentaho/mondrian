9aa0a9e71e46b6e3662529a9a2fbe1c8f48eefd9
Sherman Wood <sgwood(@)ignore.users.sf.net>
2009-04-19 06:29:29 -0800

    MONDRIAN: build updates Default log4j settings during build New cmdrunner-run target Optional mondrian.foodmart.jdbcSchema property for load-foodmart Fix for compileJdk16 under Windows Improvements for workbench targets
    [git-p4: depot-paths = "//open/mondrian/": change = 12629]

--- buildJdk16.bat
+++ buildJdk16.bat
@@ -4,13 +4,13 @@ rem Called recursively from 'ant release' to build the files which can only be
 rem built under JDK 1.6.
 
 rem Change the following line to point to your JDK 1.6 home.
-set JAVA_HOME=C:\jdk1.6.0_01
+rem set JAVA_HOME=C:\jdk1.6.0_01
 
 rem Change the following line to point to your ant home.
-set ANT_HOME=C:\open\thirdparty\ant
+rem set ANT_HOME=C:\open\thirdparty\ant
 
-set PATH=%JAVA_HOME%\bin;%PATH%
-%ANT_HOME%\bin\ant -Dskip.download=true compile.java
+rem set PATH=%JAVA_HOME%\bin;%PATH%
+"%ANT_HOME%\bin\ant" -Dskip.download=true compile.java
 
 rem End buildJdk16.bat
 
