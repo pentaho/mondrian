05445927659101eb1e0c45263ce3cf39e16b97ea
Julian Hyde <julianhyde@gmail.com>
2010-01-07 00:35:20 -0800

    MONDRIAN: Integrate from main to 3.1 changes to upgrade olap4j version, and     implement Scenario. (Some recent Scenario fixes are not included, and I     don't intend to integrate them.)
    [git-p4: depot-paths = "//open/mondrian-release/3.1/": change = 13290]

--- src/main/mondrian/olap/Connection.java
+++ src/main/mondrian/olap/Connection.java
@@ -123,44 +123,6 @@ public interface Connection {
      * @return Data source
      */
     DataSource getDataSource();
-
-    /**
-     * Creates a Scenario.
-     *
-     * <p>It does not become the active scenario for the current connection.
-     * To do this, call {@link #setScenario(Scenario)}.
-     *
-     * @see #setScenario
-     *
-     * @return a new Scenario
-     */
-    Scenario createScenario();
-
-    /**
-     * Sets the active Scenario of this connection.
-     *
-     * <p>After setting a scenario, the client may call
-     * {@link Cell#setValue} to change the value of cells returned
-     * from queries. The value of those cells is changed. This operation is
-     * referred to as 'writeback', and is used to perform 'what if' analysis,
-     * such as budgeting. See {@link mondrian.olap.Scenario} for more details.
-     *
-     * <p>If {@code scenario} is null, the connection will have no active
-     * scenario, and writeback is not allowed.
-     *
-     * <p>Scenarios are created using {@link #createScenario()}.
-     *
-     * @param scenario Scenario
-     */
-    void setScenario(Scenario scenario);
-
-    /**
-     * Returns this connection's active Scenario, or null if there is no
-     * active Scenario.
-     *
-     * @return Active scenario, or null
-     */
-    Scenario getScenario();
 }
 
 // End Connection.java
