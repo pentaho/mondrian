9ff5709e67256b347edddbc7306dbf1dba3ed29c
Julian Hyde <julianhyde@gmail.com>
2010-06-11 19:48:07 -0800

    MONDRIAN: Integration backswing.
    [git-p4: depot-paths = "//open/mondrian/": change = 13700]

--- testsrc/main/mondrian/test/SteelWheelsTestCase.java
+++ testsrc/main/mondrian/test/SteelWheelsTestCase.java
@@ -3,14 +3,19 @@
 // This software is subject to the terms of the Eclipse Public License v1.0
 // Agreement, available at the following URL:
 // http://www.eclipse.org/legal/epl-v10.html.
-// Copyright (C) 2009-2009 Julian Hyde and others
+// Copyright (C) 2009-2010 Julian Hyde and others
 // All Rights Reserved.
 // You must accept the terms of that agreement to use this software.
 */
 package mondrian.test;
 
+import java.io.InputStream;
+
 import junit.framework.TestCase;
+
 import mondrian.olap.*;
+import mondrian.rolap.RolapConnectionProperties;
+import mondrian.spi.impl.FilterDynamicSchemaProcessor;
 
 /**
  * Unit test against Pentaho's Steel Wheels sample database.
@@ -45,33 +50,39 @@ public class SteelWheelsTestCase extends TestCase {
      * different source for your FoodMart connection.
      */
     public TestContext getTestContext() {
-        return new DelegatingTestContext(
-            TestContext.instance())
-        {
-            public Util.PropertyList getFoodMartConnectionProperties() {
-                final Util.PropertyList propertyList =
-                    Util.parseConnectString(getDefaultConnectString());
-                // Assume we are talking to MySQL. Connect to 'sampledata'
-                // database, using usual credentials ('foodmart').
-                propertyList.put(
-                    "Jdbc",
-                    Util.replace(
-                        propertyList.get("Jdbc"),
-                        "/foodmart",
-                        "/steelwheels"));
-                propertyList.put(
-                    "Catalog",
-                    Util.replace(
-                        propertyList.get("Catalog"),
-                        "FoodMart.xml",
-                        "SteelWheels.mondrian.xml"));
-                return propertyList;
-            }
+        return new SteelWheelsTestContext(TestContext.instance());
+    }
 
-            public String getDefaultCubeName() {
-                return "SteelWheelsSales";
-            }
-        };
+    private static class SteelWheelsTestContext extends DelegatingTestContext
+    {
+        private SteelWheelsTestContext(TestContext testContext) {
+            super(testContext);
+        }
+
+        @Override
+        public Util.PropertyList getFoodMartConnectionProperties() {
+            final Util.PropertyList propertyList =
+                Util.parseConnectString(getDefaultConnectString());
+            // Assume we are talking to MySQL. Connect to 'sampledata'
+            // database, using usual credentials ('foodmart').
+            propertyList.put(
+                RolapConnectionProperties.Jdbc.name(),
+                Util.replace(
+                    propertyList.get(RolapConnectionProperties.Jdbc.name()),
+                    "/foodmart",
+                    "/steelwheels"));
+            propertyList.put(
+                RolapConnectionProperties.Catalog.name(),
+                Util.replace(
+                    propertyList.get(RolapConnectionProperties.Catalog.name()),
+                    "FoodMart.xml",
+                    "SteelWheels.mondrian.xml"));
+            return propertyList;
+        }
+
+        public String getDefaultCubeName() {
+            return "SteelWheelsSales";
+        }
     }
 
     /**
@@ -84,12 +95,15 @@ public class SteelWheelsTestCase extends TestCase {
         }
         testContext.assertAxisReturns(
             "Measures.Members",
-            "[Measures].[Quantity]\n" + "[Measures].[Sales]");
+            "[Measures].[Quantity]\n"
+            + "[Measures].[Sales]\n"
+            + "[Measures].[Fact Count]");
     }
 
     /**
      * Test case for Infobright issue where [Markets].[All Markets].[Japan]
      * was not found but [Markets].[All Markets].[JAPAN] was OK.
+     * (We've since dropped 'All Xxx' from member unique names.)
      */
     public void testMarkets() {
         TestContext testContext = getTestContext();
@@ -101,7 +115,7 @@ public class SteelWheelsTestCase extends TestCase {
             "Axis #0:\n"
             + "{}\n"
             + "Axis #1:\n"
-            + "{[Markets].[All Markets].[Japan]}\n"
+            + "{[Markets].[Japan]}\n"
             + "Row #0: 4,923\n");
 
         testContext.assertQueryReturns(
@@ -109,11 +123,11 @@ public class SteelWheelsTestCase extends TestCase {
             "Axis #0:\n"
             + "{}\n"
             + "Axis #1:\n"
-            + "{[Markets].[All Markets].[#null]}\n"
-            + "{[Markets].[All Markets].[APAC]}\n"
-            + "{[Markets].[All Markets].[EMEA]}\n"
-            + "{[Markets].[All Markets].[Japan]}\n"
-            + "{[Markets].[All Markets].[NA]}\n"
+            + "{[Markets].[#null]}\n"
+            + "{[Markets].[APAC]}\n"
+            + "{[Markets].[EMEA]}\n"
+            + "{[Markets].[Japan]}\n"
+            + "{[Markets].[NA]}\n"
             + "Row #0: \n"
             + "Row #0: 12,878\n"
             + "Row #0: 49,578\n"
@@ -125,14 +139,14 @@ public class SteelWheelsTestCase extends TestCase {
             "Axis #0:\n"
             + "{}\n"
             + "Axis #1:\n"
-            + "{[Markets].[All Markets].[EMEA].[UK].[Isle of Wight].[Cowes]}\n"
-            + "{[Markets].[All Markets].[Japan]}\n"
-            + "{[Markets].[All Markets].[Japan].[Hong Kong]}\n"
-            + "{[Markets].[All Markets].[Japan].[Hong Kong].[#null]}\n"
-            + "{[Markets].[All Markets].[Japan].[Hong Kong].[#null].[Central Hong Kong]}\n"
-            + "{[Markets].[All Markets].[Japan].[Japan]}\n"
-            + "{[Markets].[All Markets].[Japan].[Japan].[Osaka]}\n"
-            + "{[Markets].[All Markets].[Japan].[Japan].[Osaka].[Osaka]}\n"
+            + "{[Markets].[EMEA].[UK].[Isle of Wight].[Cowes]}\n"
+            + "{[Markets].[Japan]}\n"
+            + "{[Markets].[Japan].[Hong Kong]}\n"
+            + "{[Markets].[Japan].[Hong Kong].[#null]}\n"
+            + "{[Markets].[Japan].[Hong Kong].[#null].[Central Hong Kong]}\n"
+            + "{[Markets].[Japan].[Japan]}\n"
+            + "{[Markets].[Japan].[Japan].[Osaka]}\n"
+            + "{[Markets].[Japan].[Japan].[Osaka].[Osaka]}\n"
             + "Row #0: 895\n"
             + "Row #0: 4,923\n"
             + "Row #0: 596\n"
@@ -147,16 +161,136 @@ public class SteelWheelsTestCase extends TestCase {
             "Axis #0:\n"
             + "{}\n"
             + "Axis #1:\n"
-            + "{[Markets].[All Markets].[#null]}\n"
-            + "{[Markets].[All Markets].[APAC]}\n"
-            + "{[Markets].[All Markets].[EMEA]}\n"
-            + "{[Markets].[All Markets].[Japan]}\n"
-            + "{[Markets].[All Markets].[NA]}\n"
+            + "{[Markets].[#null]}\n"
+            + "{[Markets].[APAC]}\n"
+            + "{[Markets].[EMEA]}\n"
+            + "{[Markets].[Japan]}\n"
+            + "{[Markets].[NA]}\n"
             + "Row #0: \n"
             + "Row #0: 12,878\n"
             + "Row #0: 49,578\n"
             + "Row #0: 4,923\n"
             + "Row #0: 37,952\n");
+    }
+
+    /**
+     * Test case for bug <a href="http://jira.pentaho.com/browse/MONDRIAN-755">
+     * MONDRIAN-755, "Getting drillthrough count results in exception"</a>.
+     */
+    public void testBugMondrian755() {
+        TestContext testContext = getTestContext();
+        if (!testContext.databaseIsValid()) {
+            return;
+        }
+        // One-dimensional query, using set and trivial calc member.
+        checkCellZero(
+            testContext,
+            "With \n"
+            + "Set [*BASE_MEMBERS_Measures] as '{[Measures].[*FORMATTED_MEASURE_0]}'\n"
+            + "Member [Measures].[*FORMATTED_MEASURE_0] as '[Measures].[Sales]', FORMAT_STRING = '#,###', SOLVE_ORDER=400\n"
+            + "Select\n"
+            + "[*BASE_MEMBERS_Measures] on columns\n"
+            + "From [SteelWheelsSales]");
+
+        // One-dimensional query, using trivial calc member.
+        checkCellZero(
+            testContext,
+            "With \n"
+            + "Member [Measures].[*FORMATTED_MEASURE_0] as '[Measures].[Sales]', FORMAT_STRING = '#,###', SOLVE_ORDER=400\n"
+            + "Select\n"
+            + " {[Measures].[*FORMATTED_MEASURE_0]} on columns\n"
+            + "From [SteelWheelsSales]");
+
+        // One-dimensional query, using simple calc member.
+        checkCellZero(
+            testContext,
+            "With \n"
+            + "Member [Measures].[Avg Price] as '[Measures].[Sales] / [Measures].[Quantity]', FORMAT_STRING = '#.##'\n"
+            + "Select\n"
+            + " {[Measures].[Avg Price]} on columns\n"
+            + "From [SteelWheelsSales]");
+
+        // Zero dim query
+        checkCellZero(
+            testContext,
+            "Select\n"
+            + "From [SteelWheelsSales]");
+
+        // Zero dim query on calc member
+        checkCellZero(
+            testContext,
+            "With \n"
+            + "Member [Measures].[*FORMATTED_MEASURE_0] as '[Measures].[Sales]', FORMAT_STRING = '#,###', SOLVE_ORDER=400\n"
+            + "Select\n"
+            + "From [SteelWheelsSales]\n"
+            + "Where [Measures].[*FORMATTED_MEASURE_0]");
+
+        // Two-dimensional query, using trivial calc member.
+        checkCellZero(
+            testContext,
+            "With \n"
+            + "Member [Measures].[*FORMATTED_MEASURE_0] as '[Measures].[Sales]', FORMAT_STRING = '#,###', SOLVE_ORDER=400\n"
+            + "Select\n"
+            + " {[Measures].[*FORMATTED_MEASURE_0]} on columns,"
+            + " [Product].[All Products] * [Customers].[All Customers] on rows\n"
+            + "From [SteelWheelsSales]");
+    }
+
+    private void checkCellZero(TestContext testContext, String mdx) {
+        final Result result = testContext.executeQuery(mdx);
+        final Cell cell = result.getCell(new int[result.getAxes().length]);
+        assertTrue(cell.canDrillThrough());
+        assertEquals(2996, cell.getDrillThroughCount());
+    }
+
+    /**
+     * Test case for bug <a href="http://jira.pentaho.com/browse/MONDRIAN-756">
+     * MONDRIAN-756, "Error in RolapResult.replaceNonAllMembers leads to
+     * NPE"</a>.
+     */
+    public void testBugMondrian756() {
+        TestContext testContext =
+            new SteelWheelsTestContext(TestContext.instance()) {
+                @Override
+                public Util.PropertyList getFoodMartConnectionProperties() {
+                    Util.PropertyList propertyList =
+                        super.getFoodMartConnectionProperties();
+                    propertyList.put(
+                        RolapConnectionProperties.DynamicSchemaProcessor.name(),
+                        Mondrian756SchemaProcessor.class.getName());
+                    return propertyList;
+                }
+            };
+        if (!testContext.databaseIsValid()) {
+            return;
+        }
+        testContext.assertQueryReturns(
+            "select NON EMPTY {[Measures].[Quantity]} ON COLUMNS,\n"
+            + "NON EMPTY {[Markets].[APAC]} ON ROWS\n"
+            + "from [SteelWheelsSales]\n"
+            + "where [Time].[2004]",
+            "Axis #0:\n"
+            + "{[Time].[2004]}\n"
+            + "Axis #1:\n"
+            + "{[Measures].[Quantity]}\n"
+            + "Axis #2:\n"
+            + "{[Markets].[APAC]}\n"
+            + "Row #0: 5,938\n");
+    }
+
+    public static class Mondrian756SchemaProcessor
+        extends FilterDynamicSchemaProcessor
+    {
+        @Override
+        public String filter(
+            String schemaUrl,
+            Util.PropertyList connectInfo,
+            InputStream stream) throws Exception
+        {
+            String schema = super.filter(schemaUrl, connectInfo, stream);
+            return Util.replace(
+                schema, " hasAll=\"true\"", " hasAll=\"false\"");
+        }
     }
 }
 
