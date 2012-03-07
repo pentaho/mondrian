d3fb59b12b356409f939127369489ba4c030b407
Julian Hyde <julianhyde@gmail.com>
2012-03-06 22:05:51 -0800

    MONDRIAN: buildOnJdk.sh now uses JAVA_HOME_15, JAVA_HOME_16 and JAVA_HOME_17     environment variables, if present. This means that the script does not need     to be modified in a build environment, as long as those variables are set.     Then it looks into the machine-specific install locations. Finally, one can     customize where it looks for JDKs. If no valid JDK is found, uses the     incoming JAVA_HOME, and ant will politely no-op if it is not the version it     needs.
    [git-p4: depot-paths = "//open/mondrian/": change = 14994]

--- buildOnJdk.sh
+++ buildOnJdk.sh
@@ -7,62 +7,97 @@
 #
 # For example:
 #   buildOnJdk.sh jdk1.6 compile.java
-########################################################################
+
 jdkVersion=$1
 shift
 
-########################################################################
-# If you are building Mondrian for a single JDK (most likely the
-# case), leave the following lines commented.
-#
-# If you are building a release, you will need to compile different
-# parts of Mondrian's source code under different JDKs.  Uncomment the
-# following lines to ensure that JAVA_HOME is assigned correctly for
-# each JDK version.
-#
-########################################################################
-#case "$jdkVersion" in
-#(*) export JAVA_HOME=/usr/lib/jvm/${jdkVersion};;
-#esac
-#
-#case $(uname) in
-#Darwin)
-#    JDK_VERSION=`echo "${jdkVersion}" | sed 's/jdk\([0-9]\.[0-9]\)/\1/'`
-#    export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JDK_VERSION}/Home;;
-#*)
-#    export JAVA_HOME=/usr/lib/jvm/${jdkVersion};;
-#esac
-########################################################################
-# From this point, you don't need to modify anything... we hope.
+# Version number without jdk prefix. E.g. '1.6'.
+versionSansJdk=`echo "${jdkVersion}" | sed 's/jdk\([0-9]\.[0-9]\)/\1/'`
 
-########################################################################
-# Validate that JAVA_HOME exists
-if [ ! -d $JAVA_HOME ]; then
-   echo "$0: Invalid JAVA_HOME $JAVA_HOME; skipping compile."
-   exit 1
+# Chooses a JAVA_HOME to match the required JDK version.
+#
+# If you are building Mondrian for a single JDK (most likely the case), it
+# doesn't matter if this method cannot find a valid JAVA_HOME for the JDK
+# version. It will keep the existing JAVA_HOME, ant will report that the JDK
+# version is not the one required, and will not compile any files. You will
+# still end up with a valid build for all the classes needed by your JDK
+# version.
+#
+# If you are building a release, you must compile different parts of Mondrian's
+# source code under different JDKs.  You will need to do one of the following:
+#
+# 1. Specify environment variables JAVA_HOME_15, JAVA_HOME_16 and JAVA_HOME_17
+# before invoking 'ant'.
+#
+# 2. Install a JDK in (or create a symbolic link as) one of the standard
+# locations: /usr/lib/jvm/jdk1.x on Linux/Unix/Cygwin; or
+# /System/Library/Frameworks/JavaVM.framework/Versions/1.x/Home on MacOS.
+#
+# 3. Customize this method with the correct JDK location.
+#
+function chooseJavaHome() {
+    # If JAVA_HOME_xx is specified in the environment, use it.
+    case "$jdkVersion" in
+    (jdk1.5)
+        if [ -d "$JAVA_HOME_15" ]; then
+            export JAVA_HOME="$JAVA_HOME_15"
+            return
+        fi
+        ;;
+    (jdk1.6)
+        if [ -d "$JAVA_HOME_16" ]; then
+            export JAVA_HOME="$JAVA_HOME_16"
+            return
+        fi
+        ;;
+    (jdk1.7)
+        if [ -d "$JAVA_HOME_17" ]; then
+            export JAVA_HOME="$JAVA_HOME_17"
+            return
+        fi
+        ;;
+    esac
+
+    # 2. Look in default location based on the operating system and JDK
+    # version. If the directory exists, we presume that it is a valid JDK, and
+    # override JAVA_HOME.
+    defaultJavaHome=
+    case $(uname) in
+    (Darwin)
+        defaultJavaHome=/System/Library/Frameworks/JavaVM.framework/Versions/${versionSansJdk}/Home;;
+    (*)
+        defaultJavaHome=/usr/lib/jvm/${jdkVersion};;
+    esac
+
+    if [ -d "$defaultJavaHome" ]; then
+        export JAVA_HOME="$defaultJavaHome"
+        return
+    fi
+
+    # 3. If JDK is installed in a non-standard location, customize here.
+    #
+    #case ${jdkVersion} in
+    #(jdk1.5) export JAVA_HOME=...; return ;;
+    #(jdk1.6) export JAVA_HOME=...; return ;;
+    #(jdk1.7) export JAVA_HOME=...; return ;;
+    #esac
+
+    # 4. Leave JAVA_HOME unchanged. If it does not match the required version,
+    # ant will no-op.
+}
+
+chooseJavaHome
+
+if [ ! -d "$JAVA_HOME" ]; then
+    echo "$0: Invalid JAVA_HOME $JAVA_HOME; skipping compile."
+    exit 1
 fi
 
-########################################################################
-# Setting the PATH env variable
 export PATH=$JAVA_HOME/bin:$PATH
 
-########################################################################
-# Validating that Ant is present
-if [ ! -f "/opt/ant1.7/bin/ant" ]; then
-    ANT_BIN=`which ant`
-    if [ -z "$ANT_BIN" ]; then
-        echo "$0: Unable to locate ant binary; skipping compile."
-        exit 1
-    else
-        echo Using JAVA_HOME: $JAVA_HOME
-        echo Using Ant arguments: -Drequested.java.version="$jdkVersion" $@
-        $ANT_BIN -Drequested.java.version="$jdkVersion" "$@"
-    fi
-else
-    echo Using JAVA_HOME: $JAVA_HOME
-    echo Using Ant arguments: -Drequested.java.version="$jdkVersion" $@
-    /opt/ant1.7/bin/ant -Drequested.java.version="$jdkVersion" "$@"
-fi
+echo Using JAVA_HOME: $JAVA_HOME
+echo Using Ant arguments: -Drequested.java.version="$jdkVersion" $@
 
-########################################################################
+ant -Drequested.java.version="$jdkVersion" "$@"
+
 # End buildOnJdk.sh
