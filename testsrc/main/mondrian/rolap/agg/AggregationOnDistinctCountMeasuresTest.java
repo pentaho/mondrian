9a56b0997ca5006ba45602253503def21b06388e
Luc Boudreau <lucboudreau@gmail.com>
2012-03-21 11:18:45 -0400

    MONDRIAN: Fixes a failing test on Oracle. The measure order was inverted due to recent refactorings.

--- testsrc/main/mondrian/rolap/agg/AggregationOnDistinctCountMeasuresTest.java
+++ testsrc/main/mondrian/rolap/agg/AggregationOnDistinctCountMeasuresTest.java
@@ -792,8 +792,8 @@ public class AggregationOnDistinctCountMeasuresTest extends BatchTestCase {
         String oraTeraSqlForDetail =
             "select \"store\".\"store_state\" as \"c0\", "
             + "\"time_by_day\".\"the_year\" as \"c1\", "
-            + "count(distinct \"sales_fact_1997\".\"customer_id\") as \"m0\", "
-            + "sum(\"sales_fact_1997\".\"unit_sales\") as \"m1\" "
+            + "sum(\"sales_fact_1997\".\"unit_sales\") as \"m0\", "
+            + "count(distinct \"sales_fact_1997\".\"customer_id\") as \"m1\" "
             + "from \"store\" =as= \"store\", \"sales_fact_1997\" =as= \"sales_fact_1997\", "
             + "\"time_by_day\" =as= \"time_by_day\" "
             + "where \"sales_fact_1997\".\"store_id\" = \"store\".\"store_id\" "
