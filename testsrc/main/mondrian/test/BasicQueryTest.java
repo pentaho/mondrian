a48f27f70509996cc6e047d570ed84edd1a13171
Luc Boudreau <lucboudreau@gmail.com>
2012-04-24 10:07:49 -0400

    Adds a check for the result limit in the SegmentLoader. Before, we evaluated the crossjoins in memory, but now we evaluate the tuples at the same time as the cell results to save on computation. We had forgotten to add the result limit check there as well.
    Conflicts:
    
    	testsrc/main/mondrian/test/BasicQueryTest.java

--- testsrc/main/mondrian/test/BasicQueryTest.java
+++ testsrc/main/mondrian/test/BasicQueryTest.java
@@ -7510,6 +7510,20 @@ public class BasicQueryTest extends FoodMartTestCase {
         case 4:
         }
     }
+
+    public void testResultLimit() throws Exception {
+        propSaver.set(
+            MondrianProperties.instance().ResultLimit,
+            1000);
+        assertAxisThrows(
+            "CrossJoin([Product].[Brand Name].Members, [Gender].[Gender].Members)",
+            "Mondrian Error:Number of cell results to be read exceeded limit of (1,000)");
+        propSaver.set(
+            MondrianProperties.instance().ResultLimit,
+            5000);
+        executeQuery(
+            "select CrossJoin([Product].[Brand Name].Members, [Gender].[Gender].Members) on columns from [Sales]");
+    }
 }
 
 // End BasicQueryTest.java
