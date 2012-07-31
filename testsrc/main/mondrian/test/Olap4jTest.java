942bd8de9bd8c3a666a17e4461ae6de0cd8de10e
Julian Hyde <julianhyde@gmail.com>
2012-07-31 09:42:02 -0700

    Fix test: need hierarchy, not dimension.

--- testsrc/main/mondrian/test/Olap4jTest.java
+++ testsrc/main/mondrian/test/Olap4jTest.java
@@ -526,7 +526,7 @@ public class Olap4jTest extends FoodMartTestCase {
         final String mdx =
             "SELECT\n"
             + "NON EMPTY {Hierarchize({[Measures].[Customer Count]})} ON COLUMNS,\n"
-            + "CurrentDateMember([Time], \"\"\"[Time].[Year].[1997]\"\"\") ON ROWS\n"
+            + "CurrentDateMember([Time].[Year], \"\"\"[Time].[Year].[1997]\"\"\") ON ROWS\n"
             + "FROM [Sales 2]";
         try {
             final MdxParserFactory parserFactory =
