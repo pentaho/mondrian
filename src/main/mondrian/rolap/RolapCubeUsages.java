a8e808dd14f13bddb64d45200ea51e30e65893ec
Luc Boudreau <lucboudreau@gmail.com>
2012-08-28 15:26:17 -0400

    Woopsie.

--- src/main/mondrian/rolap/RolapCubeUsages.java
+++ src/main/mondrian/rolap/RolapCubeUsages.java
@@ -1,6 +1,17 @@
 /*
-// $Id$Id: //open/mondrian-release/3.2.2/src/main/mondrian/rolap/RolapCubeUsages.java#1 $
- */
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// Copyright (C) 2001-2002 Kana Software, Inc.
+// Copyright (C) 2001-2010 Julian Hyde and others
+// All Rights Reserved.
+// You must accept the terms of that agreement to use this software.
+//
+// jhyde, 10 August, 2001
+*/
+package mondrian.rolap;
+
+import mondrian.olap.MondrianDef;
 
 public class RolapCubeUsages {
     private MondrianDef.CubeUsages cubeUsages;
