937ba20b9a595da37c383d3090439525844bd7f0
tgf <tiago.n.ferreira@gmail.com>
2012-09-28 12:19:51 +0100

    prefer expression alias when adding from (fix Mondrian-1243: wrong alias table building member cache)

--- src/main/mondrian/rolap/RolapHierarchy.java
+++ src/main/mondrian/rolap/RolapHierarchy.java
@@ -664,7 +664,7 @@ public class RolapHierarchy extends HierarchyBase {
                     relationSubset(relation, expression.getTableAlias());
             }
         }
-        query.addFrom(subRelation, null, failIfExists);
+        query.addFrom(subRelation, expression == null ? null : expression.getTableAlias(), failIfExists);
     }
 
     /**
