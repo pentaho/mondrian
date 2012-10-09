2bdb60a26200d695656f93a48d58759fec6a7080
Julian Hyde <julianhyde@gmail.com>
2012-10-09 16:25:18 -0700

    Oops.

--- src/main/mondrian/olap/Util.java
+++ src/main/mondrian/olap/Util.java
@@ -3566,73 +3566,7 @@ public class Util extends XOMUtil {
     public static InputStream readVirtualFile(String url)
         throws IOException
     {
-        // Treat catalogUrl as an Apache VFS (Virtual File System) URL.
-        // VFS handles all of the usual protocols (http:, file:)
-        // and then some.
-        FileSystemManager fsManager = VFS.getManager();
-        if (fsManager == null) {
-            throw newError("Cannot get virtual file system manager");
-        }
-
-        // Workaround VFS bug.
-        if (url.startsWith("file://localhost")) {
-            url = url.substring("file://localhost".length());
-        }
-        if (url.startsWith("file:")) {
-            url = url.substring("file:".length());
-        }
-
-        //work around for VFS bug not closing http sockets
-        // (Mondrian-585)
-        if (url.startsWith("http")) {
-            try {
-                return new URL(url).openStream();
-            } catch (IOException e) {
-                throw newError(
-                    "Could not read URL: " + url);
-            }
-        }
-
-        File userDir = new File("").getAbsoluteFile();
-        FileObject file = fsManager.resolveFile(userDir, url);
-        FileContent fileContent = null;
-        try {
-            // Because of VFS caching, make sure we refresh to get the latest
-            // file content. This refresh may possibly solve the following
-            // workaround for defect MONDRIAN-508, but cannot be tested, so we
-            // will leave the work around for now.
-            file.refresh();
-
-            // Workaround to defect MONDRIAN-508. For HttpFileObjects, verifies
-            // the URL of the file retrieved matches the URL passed in.  A VFS
-            // cache bug can cause it to treat URLs with different parameters
-            // as the same file (e.g. http://blah.com?param=A,
-            // http://blah.com?param=B)
-            if (file instanceof HttpFileObject
-                && !file.getName().getURI().equals(url))
-            {
-                fsManager.getFilesCache().removeFile(
-                    file.getFileSystem(),  file.getName());
-
-                file = fsManager.resolveFile(userDir, url);
-            }
-
-            if (!file.isReadable()) {
-                throw newError(
-                    "Virtual file is not readable: " + url);
-            }
-
-            fileContent = file.getContent();
-        } finally {
-            file.close();
-        }
-
-        if (fileContent == null) {
-            throw newError(
-                "Cannot get virtual file content: " + url);
-        }
-
-        return fileContent.getInputStream();
+        return VIRTUAL_FILE_HANDLER.get().readVirtualFile(url);
     }
 
     public static String readVirtualFileAsString(
