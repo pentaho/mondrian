815aa7275998f80b40b0ba72cadedbf75053e85c
Luc Boudreau <lucboudreau@gmail.com>
2013-01-03 09:53:47 -0500

    Fixes a small class mistake.

--- src/main/mondrian/olap4j/MondrianOlap4jExtra.java
+++ src/main/mondrian/olap4j/MondrianOlap4jExtra.java
@@ -317,7 +317,7 @@ class MondrianOlap4jExtra implements XmlaHandler.XmlaExtra {
 
     public void flushSchemaCache(OlapConnection conn) throws OlapException {
         try {
-            conn.unwrap(Connection.class)
+            conn.unwrap(RolapConnection.class)
                 .getCacheControl(null).flushSchemaCache();
         } catch (SQLException e) {
             throw new OlapException(e);
