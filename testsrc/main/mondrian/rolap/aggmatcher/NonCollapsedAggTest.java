941afab16f239c7a5cb651c18910443b08a4a9e5
Matt Campbell <mcampbell@pentaho.com>
2013-01-04 15:32:22 -0500

    Merge of fix for MONDRIAN-1085, Mondrian does not recognize [dimension].[hierarchy] in <AggName> element for defining agg tables, even if SsasCompatibleNaming=true
    Conflicts:
    
    	testsrc/main/mondrian/rolap/aggmatcher/NonCollapsedAggTest.java

--- testsrc/main/mondrian/rolap/aggmatcher/NonCollapsedAggTest.java
+++ testsrc/main/mondrian/rolap/aggmatcher/NonCollapsedAggTest.java
@@ -155,6 +155,83 @@ public class NonCollapsedAggTest extends AggTableTestCase {
         + "   <Measure name=\"Unit Sales\" column=\"unit_sales\" aggregator=\"sum\" formatString=\"Standard\" />\n"
         + "</Cube>\n";
 
+    private static final String SSAS_COMPAT_CUBE = "<Cube name=\"testSsas\">\n"
+        + "    <Table name=\"foo_fact\">\n"
+        + "        <AggName name=\"agg_tenant\">\n"
+        + "            <AggFactCount column=\"fact_count\"/>\n"
+        + "            <AggMeasure name=\"[Measures].[Unit Sales]\" column=\"unit_sales\"/>\n"
+        + "            <AggLevel name=\"[dimension].[tenant].[tenant]\"\n"
+        + "                column=\"tenant_id\" collapsed=\"false\"/>\n"
+        + "        </AggName>\n"
+        + "        <AggName name=\"agg_line_class\">\n"
+        + "            <AggFactCount column=\"fact_count\"/>\n"
+        + "            <AggMeasure name=\"[Measures].[Unit Sales]\" column=\"unit_sales\"/>\n"
+        + "            <AggLevel name=\"[dimension].[distributor].[line class]\"\n"
+        + "                column=\"line_class_id\" collapsed=\"false\"/>\n"
+        + "        </AggName>\n"
+        + "        <AggName name=\"agg_line_class\">\n"
+        + "            <AggFactCount column=\"fact_count\"/>\n"
+        + "            <AggMeasure name=\"[Measures].[Unit Sales]\" column=\"unit_sales\"/>\n"
+        + "            <AggLevel name=\"[dimension].[network].[line class]\"\n"
+        + "                column=\"line_class_id\" collapsed=\"false\"/>\n"
+        + "        </AggName>\n"
+        + "    </Table>\n"
+        + "    <Dimension name=\"dimension\" foreignKey=\"line_id\">\n"
+        + "        <Hierarchy name=\"tenant\" hasAll=\"true\" allMemberName=\"All tenants\"\n"
+        + "            primaryKey=\"line_id\" primaryKeyTable=\"line\">\n"
+        + "            <Join leftKey=\"line_id\" rightKey=\"line_id\"\n"
+        + "                rightAlias=\"line_tenant\">\n"
+        + "                <Table name=\"line\"/>\n"
+        + "                <Join leftKey=\"tenant_id\" rightKey=\"tenant_id\">\n"
+        + "                    <Table name=\"line_tenant\"/>\n"
+        + "                    <Table name=\"tenant\"/>\n"
+        + "                </Join>\n"
+        + "            </Join>\n"
+        + "            <Level name=\"tenant\" table=\"tenant\" column=\"tenant_id\" nameColumn=\"tenant_name\" uniqueMembers=\"true\"/>\n"
+        + "            <Level name=\"line\" table=\"line\" column=\"line_id\" nameColumn=\"line_name\"/>\n"
+        + "        </Hierarchy>\n"
+        + "        <Hierarchy name=\"distributor\" hasAll=\"true\" allMemberName=\"All distributors\"\n"
+        + "            primaryKey=\"line_id\" primaryKeyTable=\"line\">\n"
+        + "            <Join leftKey=\"line_id\" rightKey=\"line_id\" rightAlias=\"line_line_class\">\n"
+        + "                <Table name=\"line\"/>\n"
+        + "                <Join leftKey=\"line_class_id\" rightKey=\"line_class_id\" rightAlias=\"line_class\">\n"
+        + "                    <Table name=\"line_line_class\"/>\n"
+        + "                    <Join leftKey=\"line_class_id\" rightKey=\"line_class_id\" rightAlias=\"line_class_distributor\">\n"
+        + "                        <Table name=\"line_class\"/>\n"
+        + "                        <Join leftKey=\"distributor_id\" rightKey=\"distributor_id\">\n"
+        + "                            <Table name=\"line_class_distributor\"/>\n"
+        + "                            <Table name=\"distributor\"/>\n"
+        + "                        </Join>\n"
+        + "                    </Join>\n"
+        + "                </Join>\n"
+        + "            </Join>\n"
+        + "            <Level name=\"distributor\" table=\"distributor\" column=\"distributor_id\" nameColumn=\"distributor_name\"/>\n"
+        + "            <Level name=\"line class\" table=\"line_class\" column=\"line_class_id\" nameColumn=\"line_class_name\" uniqueMembers=\"true\"/>\n"
+        + "            <Level name=\"line\" table=\"line\" column=\"line_id\" nameColumn=\"line_name\"/>\n"
+        + "        </Hierarchy>\n"
+        + "        <Hierarchy name=\"network\" hasAll=\"true\" allMemberName=\"All networks\"\n"
+        + "            primaryKey=\"line_id\" primaryKeyTable=\"line\">\n"
+        + "            <Join leftKey=\"line_id\" rightKey=\"line_id\" rightAlias=\"line_line_class\">\n"
+        + "                <Table name=\"line\"/>\n"
+        + "                <Join leftKey=\"line_class_id\" rightKey=\"line_class_id\" rightAlias=\"line_class\">\n"
+        + "                    <Table name=\"line_line_class\"/>\n"
+        + "                    <Join leftKey=\"line_class_id\" rightKey=\"line_class_id\" rightAlias=\"line_class_network\">\n"
+        + "                        <Table name=\"line_class\"/>\n"
+        + "                        <Join leftKey=\"network_id\" rightKey=\"network_id\">\n"
+        + "                            <Table name=\"line_class_network\"/>\n"
+        + "                            <Table name=\"network\"/>\n"
+        + "                        </Join>\n"
+        + "                    </Join>\n"
+        + "                </Join>\n"
+        + "            </Join>\n"
+        + "            <Level name=\"network\" table=\"network\" column=\"network_id\" nameColumn=\"network_name\"/>\n"
+        + "            <Level name=\"line class\" table=\"line_class\" column=\"line_class_id\" nameColumn=\"line_class_name\" uniqueMembers=\"true\"/>\n"
+        + "            <Level name=\"line\" table=\"line\" column=\"line_id\" nameColumn=\"line_name\"/>\n"
+        + "        </Hierarchy>\n"
+        + " </Dimension>\n"
+        + "   <Measure name=\"Unit Sales\" column=\"unit_sales\" aggregator=\"sum\" formatString=\"Standard\" />\n"
+        + "</Cube>\n";
+
     public NonCollapsedAggTest() {
         super();
     }
@@ -308,6 +385,29 @@ public class NonCollapsedAggTest extends AggTableTestCase {
 
         context.executeQuery(query1);
         context.executeQuery(query2);
+    }
+
+    public void testSsasCompatNamingInAgg() throws Exception {
+        // MONDRIAN-1085
+        if (!MondrianProperties.instance().SsasCompatibleNaming.get()) {
+            return;
+        }
+
+        final TestContext testContext = TestContext.instance().create(
+            null, SSAS_COMPAT_CUBE, null, null, null, null);
+        final String mdx =
+                "select {[Measures].[Unit Sales]} on columns, {[dimension].[tenant].[tenant].Members} on rows from [testSsas]";
+        testContext.assertQueryReturns(
+            mdx,
+            "Axis #0:\n"
+            + "{}\n"
+            + "Axis #1:\n"
+            + "{[Measures].[Unit Sales]}\n"
+            + "Axis #2:\n"
+            + "{[dimension].[tenant].[tenant one]}\n"
+            + "{[dimension].[tenant].[tenant two]}\n"
+            + "Row #0: 31\n"
+            + "Row #1: 121\n");
     }
 }
 // End NonCollapsedAggTest.java
