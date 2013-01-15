781a57481d44f01a056fa558c74cef1e618adbf1
Matt Campbell <mcampbell@pentaho.com>
2013-01-15 14:24:31 -0500

    Ugh.  More @Override attributes.

--- testsrc/main/mondrian/test/DialectTest.java
+++ testsrc/main/mondrian/test/DialectTest.java
@@ -1279,117 +1279,94 @@ public class DialectTest extends TestCase {
         }
 
 
-        @Override
         public int getPrecision(int column) throws SQLException {
             return precision;
         }
 
-        @Override
         public String getColumnName(int column) throws SQLException {
             return columnName;
         }
 
-        @Override
         public int getColumnType(int column) throws SQLException {
             return columnType;
         }
 
-        @Override
         public int getScale(int column) throws SQLException {
             return scale;
         }
 
-        @Override
         public int getColumnCount() throws SQLException {
             return 0;
         }
 
-        @Override
         public boolean isAutoIncrement(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public boolean isCaseSensitive(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public boolean isSearchable(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public boolean isCurrency(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public int isNullable(int column) throws SQLException {
             return 0;
         }
 
-        @Override
         public boolean isSigned(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public int getColumnDisplaySize(int column) throws SQLException {
             return 0;
         }
 
-        @Override
         public String getColumnLabel(int column) throws SQLException {
             return null;
         }
 
-        @Override
         public String getSchemaName(int column) throws SQLException {
             return null;
         }
 
-        @Override
         public String getTableName(int column) throws SQLException {
             return null;
         }
 
-        @Override
         public String getCatalogName(int column) throws SQLException {
             return null;
         }
 
-        @Override
         public String getColumnTypeName(int column) throws SQLException {
             return null;
         }
 
-        @Override
         public boolean isReadOnly(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public boolean isWritable(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public boolean isDefinitelyWritable(int column) throws SQLException {
             return false;
         }
 
-        @Override
         public String getColumnClassName(int column) throws SQLException {
             return null;
         }
 
-        @Override
         public <T> T unwrap(Class<T> iface) throws SQLException {
             return null;
         }
 
-        @Override
         public boolean isWrapperFor(Class<?> iface) throws SQLException {
             return false;
         }
