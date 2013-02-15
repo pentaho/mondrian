a4f5736cbd36cdace1153b325abfd34f6daf5720
Curtis Boyden <cboyden@pentaho.com>
2013-02-15 15:17:19 -0500

    Created ImpalaDialect

--- src/main/mondrian/spi/Dialect.java
+++ src/main/mondrian/spi/Dialect.java
@@ -4,7 +4,7 @@
 // http://www.eclipse.org/legal/epl-v10.html.
 // You must accept the terms of that agreement to use this software.
 //
-// Copyright (C) 2008-2012 Pentaho
+// Copyright (C) 2008-2013 Pentaho
 // All Rights Reserved.
 */
 package mondrian.spi;
@@ -820,6 +820,7 @@ public interface Dialect {
         GREENPLUM,
         HIVE,
         HSQLDB,
+        IMPALA,
         INFORMIX,
         INFOBRIGHT,
         INGRES,
