a75e3d196889ba5cd598d4ae9ef95afd78379ad8
Luc Boudreau <lucboudreau@gmail.com>
2013-02-18 10:19:15 -0500

    More fixes for MONDRIAN-1385 and leaks. The class SoftSmartCache was managing its own ReferenceQueue (badly at that) and failed to clean itself up. This was done to circumvent the problems with JDK 1.4 compatibility. I've refactored it all so that it uses commons-collections, which is compatible with 1.1+. I have also integrated the comments from Julian following my previous fix in MonitorImpl so that the number of executions maintained in history is controlled via a mondrian property instead of being hardcoded.

--- src/main/mondrian/rolap/cache/SoftSmartCache.java
+++ src/main/mondrian/rolap/cache/SoftSmartCache.java
@@ -5,87 +5,43 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 2004-2005 TONBELLER AG
-// Copyright (C) 2006-2012 Pentaho
+// Copyright (C) 2006-2013 Pentaho
 // All Rights Reserved.
 */
 package mondrian.rolap.cache;
 
-import mondrian.util.Pair;
+import org.apache.commons.collections.map.ReferenceMap;
 
-import java.lang.ref.ReferenceQueue;
-import java.lang.ref.SoftReference;
 import java.util.*;
 
 /**
- * A map with soft references that is cleaned up in regular intervals.
- * <p>
- * There is no contains(key) method because it makes no sense - after
- * contains() returns true, the garbage collector may remove
- * the value that was contained. Instead the code should call get() and
- * keep a reference to the value to prevent garbage collection.
+ * An implementation of {@link SmartCacheImpl} which uses a
+ * {@link ReferenceMap} as a backing object. Both the key
+ * and the value are soft references, because of their
+ * cyclic nature.
  *
- * @author av
+ * <p>This class does not enforce any synchronization, because
+ * this is handled by {@link SmartCacheImpl}.
+ *
+ * @author av, lboudreau
  * @since Nov 3, 2005
  */
 public class SoftSmartCache<K, V> extends SmartCacheImpl<K, V> {
 
-    private final Map<K, CacheReference> cache =
-        new HashMap<K, CacheReference>();
-
-    private final ReferenceQueue<V> queue = new ReferenceQueue<V>();
-
-    /**
-     * an entry in the cache that contains the key for
-     * the cache map to remove the entry when its value
-     * has been garbage collected
-     *
-     * @author rk
-     * @since Nov 7, 2005
-     */
-    class CacheReference extends SoftReference<V> {
-        K key;
-
-        public CacheReference(K key, V value) {
-            super(value, queue);
-            this.key = key;
-        }
-
-        public String toString() {
-            return String.valueOf(get());
-        }
-    }
+    @SuppressWarnings("unchecked")
+    private final Map<K, V> cache =
+        new ReferenceMap(ReferenceMap.SOFT, ReferenceMap.SOFT);
 
     public V putImpl(K key, V value) {
-        // remove garbage collected entries from cache
-        CacheReference ref;
-        while ((ref = (CacheReference) queue.poll()) != null) {
-            cache.remove(ref.key);
-        }
-
-        // put new entry into cache
-        ref = new CacheReference(key, value);
-        ref = cache.put(key, ref);
-        if (ref != null) {
-            return ref.get();
-        }
-        return null;
+        return cache.put(key, value);
     }
 
     public V getImpl(K key) {
-        CacheReference ref = cache.get(key);
-        if (ref == null) {
-            return null;
-        }
-        V value = ref.get();
-        if (value == null) {
-            cache.remove(key);
-        }
-        return value;
+        return cache.get(key);
     }
 
     public V removeImpl(K key) {
-        final CacheReference ref = cache.remove(key);
-        return ref == null ? null : ref.get();
+        return cache.remove(key);
     }
 
     public void clearImpl() {
@@ -97,40 +53,7 @@ public class SoftSmartCache<K, V> extends SmartCacheImpl<K, V> {
     }
 
     public Iterator<Map.Entry<K, V>> iteratorImpl() {
-        final Iterator<Map.Entry<K, CacheReference>> cacheIterator =
-            cache.entrySet().iterator();
-        return new Iterator<Map.Entry<K, V>>() {
-            private Map.Entry<K, V> entry;
-
-            public boolean hasNext() {
-                if (entry != null) {
-                    return true;
-                }
-                while (cacheIterator.hasNext()) {
-                    Map.Entry<K, CacheReference> cacheEntry =
-                        cacheIterator.next();
-                    // skip over entries that have been garbage collected
-                    final V value = cacheEntry.getValue().get();
-                    if (value != null) {
-                        // Would use AbstractMap.SimpleEntry but it's not public
-                        // until JDK 1.6.
-                        entry = new Pair<K, V>(cacheEntry.getKey(), value);
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-            public Map.Entry<K, V> next() {
-                Map.Entry<K, V> entry = this.entry;
-                this.entry = null;
-                return entry;
-            }
-
-            public void remove() {
-                cacheIterator.remove();
-            }
-        };
+        return cache.entrySet().iterator();
     }
 }
 
