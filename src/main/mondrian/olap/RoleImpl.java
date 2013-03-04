08c00d6776de5da12e47d8a03941a4617295abe7
Kurtis Walker <kwalker@pentaho.com>
2013-03-04 09:53:06 -0500

    MONDRIAN-1434 - Restrictions on the measures dimension applied to measures on all cubes and not just the cube it was specified for.  Corrected by retrieving the dimension grants using identity compare instead of equality. (cherry-picked from ed59e8a)

--- src/main/mondrian/olap/RoleImpl.java
+++ src/main/mondrian/olap/RoleImpl.java
@@ -187,7 +187,7 @@ public class RoleImpl implements Role {
     public Access getAccess(Dimension dimension) {
         assert dimension != null;
         // Check for explicit rules.
-        Access access = dimensionGrants.get(dimension);
+        Access access = getDimensionGrant(dimension);
         if (access == Access.CUSTOM) {
             // For legacy reasons, if there are no accessible hierarchies
             // and the dimension has an access level of custom, we deny.
@@ -234,6 +234,19 @@ public class RoleImpl implements Role {
         }
     }
 
+    private Access getDimensionGrant(final Dimension dimension) {
+        if (dimension.isMeasures()) {
+            for (Dimension key : dimensionGrants.keySet()) {
+                if (key == dimension) {
+                    return dimensionGrants.get(key);
+                }
+            }
+            return null;
+        } else {
+            return dimensionGrants.get(dimension);
+        }
+    }
+
     /**
      * This method is used to check if the access rights over a dimension
      * that might be inherited from the parent cube.
@@ -242,7 +255,8 @@ public class RoleImpl implements Role {
      * argument.
      */
     private Access checkDimensionAccessByCubeInheritance(Dimension dimension) {
-        assert !dimensionGrants.containsKey(dimension);
+        assert dimensionGrants.containsKey(dimension) == false
+               || dimension.isMeasures();
         for (Map.Entry<Cube, Access> cubeGrant : cubeGrants.entrySet()) {
             final Access access = toAccess(cubeGrant.getValue());
             // The 'none' and 'custom' access level are not good enough
