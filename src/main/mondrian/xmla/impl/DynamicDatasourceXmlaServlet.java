32e653b3264a7f6af4f9d26816dbf04d772148fb
Luc Boudreau <lucboudreau@gmail.com>
2013-05-02 14:39:21 -0400

    Small fix for MONDRIAN-1566. The DynamicDataSourceServlet was creating a new DynamicContentFinder for each request creating a new Timer thread each time.

--- src/main/mondrian/xmla/impl/DynamicDatasourceXmlaServlet.java
+++ src/main/mondrian/xmla/impl/DynamicDatasourceXmlaServlet.java
@@ -4,13 +4,16 @@
 // http://www.eclipse.org/legal/epl-v10.html.
 // You must accept the terms of that agreement to use this software.
 //
-// Copyright (C) 2006-2011 Pentaho
+// Copyright (C) 2006-2013 Pentaho
 // All Rights Reserved.
 */
 package mondrian.xmla.impl;
 
 import mondrian.server.DynamicContentFinder;
 import mondrian.server.RepositoryContentFinder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Extends DefaultXmlaServlet to add dynamic datasource loading capability.
@@ -19,11 +22,27 @@ import mondrian.server.RepositoryContentFinder;
  * @author Luc Boudreau
  */
 public class DynamicDatasourceXmlaServlet extends MondrianXmlaServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * A map of datasources definitions to {@link DynamicContentFinder}
+     * instances.
+     */
+    private final Map<String, DynamicContentFinder> finders =
+        new HashMap<String, DynamicContentFinder>();
+
+    @Override
     protected RepositoryContentFinder makeContentFinder(String dataSources) {
-        return new DynamicContentFinder(dataSources);
+        if (!finders.containsKey(dataSources)) {
+            finders.put(dataSources, new DynamicContentFinder(dataSources));
+        }
+        return finders.get(dataSources);
     }
     @Override
     public void destroy() {
+        for (DynamicContentFinder finder : finders.values()) {
+            finder.shutdown();
+        }
         super.destroy();
     }
 }
