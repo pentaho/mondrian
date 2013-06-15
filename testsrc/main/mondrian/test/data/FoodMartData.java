cade22bd9539cd8082495f8e77e18e8bd64c2fd4
Julian Hyde <julianhyde@gmail.com>
2013-06-15 15:00:22 -0700

    Add FoodMartData. Allows other projects to access resources in jar files, such as queries.json, via the class loader.

---
+++ testsrc/main/mondrian/test/data/FoodMartData.java
@@ -0,0 +1,32 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (C) 2013-2013 Pentaho
+// All Rights Reserved.
+*/
+package mondrian.test.data;
+
+import java.io.*;
+
+/**
+ * Loads the FoodMart queries data set.
+ *
+ * <p>Used in maven resource {groupId=pentaho,
+ * artifactId=mondrian-data-foodmart-queries}.</p>
+ */
+public class FoodMartData {
+    public FoodMartData() {
+    }
+
+    /** Returns the contents of "queries.json" in the current jar file as a
+     * stream. */
+    public InputStream getQueries() throws IOException {
+        return FoodMartData.class.getClassLoader()
+            .getResourceAsStream("queries.json");
+    }
+}
+
+// End FoodMartData.java
