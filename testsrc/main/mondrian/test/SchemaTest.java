ffc61abf86dcdf113c4eea46c67e4bd4e1bfddb6
Kurtis Walker <kwalker@pentaho.com>
2014-03-26 17:52:40 -0400

    [MONDRIAN-1961] - multi-byte charaters in catalog files were not getting read into strings correctly.  Now using IOUtils to read the InputStream into a String.

--- testsrc/main/mondrian/test/SchemaTest.java
+++ testsrc/main/mondrian/test/SchemaTest.java
@@ -4,7 +4,7 @@
 // http://www.eclipse.org/legal/epl-v10.html.
 // You must accept the terms of that agreement to use this software.
 //
-// Copyright (C) 2006-2012 Pentaho
+// Copyright (C) 2006-2014 Pentaho
 // All Rights Reserved.
 */
 package mondrian.test;
@@ -13,18 +13,19 @@ import mondrian.olap.*;
 import mondrian.olap.Category;
 import mondrian.olap.Hierarchy;
 import mondrian.olap.Level;
-import mondrian.rolap.TupleReader;
+import mondrian.rolap.*;
 import mondrian.rolap.aggmatcher.AggTableManager;
 import mondrian.spi.Dialect;
 import mondrian.spi.PropertyFormatter;
 import mondrian.util.Bug;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.*;
 import org.apache.log4j.varia.LevelRangeFilter;
 
 import org.olap4j.metadata.NamedList;
 
-import java.io.StringWriter;
+import java.io.*;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -4645,6 +4646,34 @@ public class SchemaTest extends FoodMartTestCase {
             + "[Store Size in SQFT].[38382], "
             + "[Store Size in SQFT].[39696]]",
             members.toString());
+    }
+
+    public void testMultiByteSchemaReadFromFile() throws IOException {
+        String rawSchema = TestContext.getRawFoodMartSchema().replace(
+            "<Hierarchy hasAll=\"true\" allMemberName=\"All Gender\" primaryKey=\"customer_id\">",
+            "<Hierarchy name=\"地域\" hasAll=\"true\" allMemberName=\"All Gender\" primaryKey=\"customer_id\">");
+        File schemaFile = File.createTempFile("multiByteSchema", ",xml");
+        schemaFile.deleteOnExit();
+        FileOutputStream output = new FileOutputStream(schemaFile);
+        IOUtils.write(rawSchema, output);
+        IOUtils.closeQuietly(output);
+        TestContext context = getTestContext();
+        final Util.PropertyList properties =
+            context.getConnectionProperties().clone();
+        properties.put(
+            RolapConnectionProperties.Catalog.name(),
+            schemaFile.getAbsolutePath());
+        context.withProperties(properties).assertQueryReturns(
+            "select [Gender].members on 0 from sales",
+            "Axis #0:\n"
+            + "{}\n"
+            + "Axis #1:\n"
+            + "{[Gender.地域].[All Gender]}\n"
+            + "{[Gender.地域].[F]}\n"
+            + "{[Gender.地域].[M]}\n"
+            + "Row #0: 266,773\n"
+            + "Row #0: 131,558\n"
+            + "Row #0: 135,215\n");
     }
 }
 
