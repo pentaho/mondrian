668943c7111ca67276bb6dd0fbf71e335cce584f
Luc Boudreau <lucboudreau@gmail.com>
2014-04-16 13:03:58 -0400

    [CELANUP] Must mock this. Was throwing UME, must throw SQLE.

--- testsrc/main/mondrian/test/TestContext.java
+++ testsrc/main/mondrian/test/TestContext.java
@@ -2107,6 +2107,13 @@ public class TestContext {
         public DatabaseMetaData getMetaData() {
             return metaData;
         }
+
+        /**
+         * Proxy for {@link java.sql.Connection#createStatement()}
+         */
+        public Statement createStatement() throws SQLException {
+            throw new SQLException();
+        }
     }
 
     // Public only because required for reflection to work.
