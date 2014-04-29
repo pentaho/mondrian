20a88f96e2486cd23bab8bca58c2dde186b285f2
Luc Boudreau <lucboudreau@gmail.com>
2014-04-29 11:59:33 -0400

    [MONDRIAN-1998] When GC'ins a schema, we only clear the JDBC caches. When flushing the entire schema, we have to explicitly flush the segments from the indexes or else the external caches cannot get notified that the contents has become stale.

--- src/main/mondrian/rolap/RolapSchema.java
+++ src/main/mondrian/rolap/RolapSchema.java
@@ -5,10 +5,9 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 2001-2005 Julian Hyde
-// Copyright (C) 2005-2013 Pentaho and others
+// Copyright (C) 2005-2014 Pentaho and others
 // All Rights Reserved.
 */
-
 package mondrian.rolap;
 
 import mondrian.olap.*;
@@ -317,17 +316,43 @@ public class RolapSchema implements Schema {
         }
     }
 
-    protected void finalCleanUp() {
+    protected void flushSegments() {
+        final RolapConnection internalConnection = getInternalConnection();
+        if (internalConnection != null) {
+            final CacheControl cc = internalConnection.getCacheControl(null);
+            for (RolapCube cube : getCubeList()) {
+                cc.flush(cc.createMeasuresRegion(cube));
+            }
+        }
+    }
+
+    /**
+     * Clears the cache of JDBC tables for the aggs.
+     */
+    protected void flushJdbcSchema() {
+        // Cleanup the agg table manager's caches.
         if (aggTableManager != null) {
             aggTableManager.finalCleanUp();
             aggTableManager = null;
         }
     }
 
+    /**
+     * Performs a sweep of the JDBC tables caches and the segment data.
+     * Only called internally when a schema and it's data must be refreshed.
+     */
+    protected void finalCleanUp() {
+        // Cleanup the segment data.
+        flushSegments();
+        // Cleanup the agg JDBC cache
+        flushJdbcSchema();
+    }
+
     protected void finalize() throws Throwable {
         try {
             super.finalize();
-            finalCleanUp();
+            // Only clear the JDBC cache to prevent leaks.
+            flushJdbcSchema();
         } catch (Throwable t) {
             LOGGER.info(
                 MondrianResource.instance()
