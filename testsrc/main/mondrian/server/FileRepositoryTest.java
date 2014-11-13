024270c43503869f80b914d258b980959f203e45
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2014-11-13 19:37:58 +0300

    changing according to checkstyle

--- testsrc/main/mondrian/server/FileRepositoryTest.java
+++ testsrc/main/mondrian/server/FileRepositoryTest.java
@@ -1,57 +1,69 @@
+/*
+* This software is subject to the terms of the Eclipse Public License v1.0
+* Agreement, available at the following URL:
+* http://www.eclipse.org/legal/epl-v10.html.
+* You must accept the terms of that agreement to use this software.
+*
+* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+*/
 package mondrian.server;
+
+import junit.framework.TestCase;
 
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import junit.framework.TestCase;
 
 public class FileRepositoryTest extends TestCase {
 
-	public void testGetServerInfo_ifGetDataSourceInfoIsCalled() {
+     public void testGetServerInfo_ifGetDataSourceInfoIsCalled() {
+        final String datasourceNameStub = "DATASOURCE_NAME";
+        final String datasourceInfoStub = "DATASOURCE_INFO";
+        final String contentStub = "<?xml version=\"1.0\""
+        + " encoding=\"UTF-8\"?>\n"
+                + "<DataSources>\n"
+                + "<DataSource>\n"
+                + "<DataSourceName>"
+                + datasourceNameStub
+                + "</DataSourceName>\n"
+                + "<DataSourceDescription>Pentaho BI Platform Datasources</DataSourceDescription>\n"
+                + "<URL>/pentaho/Xmla</URL>\n"
+                + "<DataSourceInfo>"
+                + datasourceInfoStub
+                + "</DataSourceInfo>\n"
+                + "<ProviderName>PentahoXMLA</ProviderName>\n"
+                + "<ProviderType>MDP</ProviderType>\n"
+                + "<AuthenticationMode>Unauthenticated</AuthenticationMode>\n"
+                + "<Catalogs>\n"
+                + "<Catalog name=\"SampleData\">\n"
+                + "<DataSourceInfo>DataSource=SampleData;EnableXmla=false;Provider=mondrian;Datasource=\"SampleData\";overwrite=\"true\"</DataSourceInfo>\n"
+                + "<Definition>mondrian:/SampleData</Definition>\n"
+                + "</Catalog>\n"
+                + "</Catalogs>\n"
+                + "</DataSource>\n"
+                + "</DataSources>\n";
 
-		final String datasourceNameStub = "DATASOURCE_NAME";
-		final String datasourceInfoStub = "DATASOURCE_INFO";
-		final String contentStub = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-				+ "<DataSources>\n"
-				+ "<DataSource>\n"
-				+ "<DataSourceName>"
-				+ datasourceNameStub
-				+ "</DataSourceName>\n"
-				+ "<DataSourceDescription>Pentaho BI Platform Datasources</DataSourceDescription>\n"
-				+ "<URL>/pentaho/Xmla</URL>\n"
-				+ "<DataSourceInfo>"
-				+ datasourceInfoStub
-				+ "</DataSourceInfo>\n"
-				+ "<ProviderName>PentahoXMLA</ProviderName>\n"
-				+ "<ProviderType>MDP</ProviderType>\n"
-				+ "<AuthenticationMode>Unauthenticated</AuthenticationMode>\n"
-				+ "<Catalogs>\n"
-				+ "<Catalog name=\"SampleData\">\n"
-				+ "<DataSourceInfo>DataSource=SampleData;EnableXmla=false;Provider=mondrian;Datasource=\"SampleData\";overwrite=\"true\"</DataSourceInfo>\n"
-				+ "<Definition>mondrian:/SampleData</Definition>\n"
-				+ "</Catalog>\n"
-				+ "</Catalogs>\n"
-				+ "</DataSource>\n"
-				+ "</DataSources>\n";
+        FileRepository fileRepositoryMock = mock(FileRepository.class);
+        RepositoryContentFinder repositoryContentFinderMock = mock(
+                RepositoryContentFinder.class);
 
-		FileRepository fileRepositoryMock = mock(FileRepository.class);
-		RepositoryContentFinder repositoryContentFinderMock = mock(RepositoryContentFinder.class);
+        doReturn(repositoryContentFinderMock).when(fileRepositoryMock)
+                .getRepositoryContentFinder();
+        doReturn(contentStub).when(repositoryContentFinderMock).getContent();
 
-		doReturn(repositoryContentFinderMock).when(fileRepositoryMock)
-				.getRepositoryContentFinder();
-		doReturn(contentStub).when(repositoryContentFinderMock).getContent();
+        doCallRealMethod().when(fileRepositoryMock).getServerInfo();
 
-		doCallRealMethod().when(fileRepositoryMock).getServerInfo();
+        FileRepository.ServerInfo serverInfo = fileRepositoryMock
+                .getServerInfo();
 
-		FileRepository.ServerInfo serverInfo = fileRepositoryMock
-				.getServerInfo();
+        // in reality DATASOURCE_NAME is Provider=Mondrian for example
+        FileRepository.DatabaseInfo databaseInfo = (FileRepository
+                .DatabaseInfo) serverInfo
+                          .getDatasourceMap().get(datasourceNameStub);
 
-		// in reality DATASOURCE_NAME is Provider=Mondrian for example
-		FileRepository.DatabaseInfo databaseInfo = (FileRepository.DatabaseInfo) serverInfo
-				.getDatasourceMap().get(datasourceNameStub);
-
-		assertTrue(databaseInfo.getProperties().containsValue(
-				datasourceInfoStub));
-	}
+        assertTrue(databaseInfo.getProperties().containsValue(datasourceInfoStub));
+     }
 
 }
+
+//End FileRepositoryTest.java
