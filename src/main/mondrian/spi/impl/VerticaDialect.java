9972f14090ba95e6bda1b9527d5aad5a9ec6e3eb
tgf <tiago.n.ferreira@gmail.com>
2014-12-12 15:54:47 +0000

    [MONDRIAN-1028] vertica dialect fixes
    from evergreen
    - e69470d60916e0588955f4a5d8d5947aa15d6f18 Fixed issues found while
    running Vertica test suite. SegmentLoader has issues with Null Doubles,
    and the Vertica Dialect doesn't handle Longs well.
    - also minor dialect changes
    d5ed2859e10ed623575997f5d2e3f9bcbce10f27
    1db658ec395198608ebb02648f730ab32781ad6c

--- src/main/mondrian/spi/impl/VerticaDialect.java
+++ src/main/mondrian/spi/impl/VerticaDialect.java
@@ -10,8 +10,14 @@ package mondrian.spi.impl;
 
 import mondrian.rolap.SqlStatement;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Implementation of {@link mondrian.spi.Dialect} for the Vertica database.
@@ -49,6 +55,11 @@ public class VerticaDialect extends JdbcDialectImpl {
     }
 
     @Override
+    public boolean allowsMultipleCountDistinct() {
+        return false;
+    }
+
+    @Override
     public boolean supportsResultSetConcurrency(int type, int concurrency) {
         return false;
     }
@@ -62,16 +73,57 @@ public class VerticaDialect extends JdbcDialectImpl {
             columnNames, columnTypes, valueList, null, false);
     }
 
+    private static final Map<Integer, SqlStatement.Type> VERTICA_TYPE_MAP;
+    static {
+        Map<Integer, SqlStatement.Type> typeMapInitial =
+            new HashMap<Integer, SqlStatement.Type>();
+        typeMapInitial.put(Types.SMALLINT, SqlStatement.Type.LONG);
+        typeMapInitial.put(Types.TINYINT, SqlStatement.Type.LONG);
+        typeMapInitial.put(Types.INTEGER, SqlStatement.Type.LONG);
+        typeMapInitial.put(Types.BOOLEAN, SqlStatement.Type.INT);
+        typeMapInitial.put(Types.DOUBLE, SqlStatement.Type.DOUBLE);
+        typeMapInitial.put(Types.FLOAT, SqlStatement.Type.DOUBLE);
+        typeMapInitial.put(Types.BIGINT, SqlStatement.Type.LONG);
+        VERTICA_TYPE_MAP = Collections.unmodifiableMap(typeMapInitial);
+    }
+
+    @Override
     public SqlStatement.Type getType(
         ResultSetMetaData metaData, int columnIndex)
         throws SQLException
     {
-        // BIGINT->LONG should be the general rule, not just for Vertica,
-        // see MONDRIAN-1890
-        return metaData.getColumnType(columnIndex + 1) == Types.BIGINT
-            ? SqlStatement.Type.LONG : super.getType(metaData, columnIndex);
+        final int columnType = metaData.getColumnType(columnIndex + 1);
+
+        SqlStatement.Type internalType = null;
+        // all int types in vertica are longs.
+        if (columnType == Types.NUMERIC || columnType == Types.DECIMAL) {
+            final int precision = metaData.getPrecision(columnIndex + 1);
+            final int scale = metaData.getScale(columnIndex + 1);
+            if (scale == 0 && precision <= 9) {
+                // An int (up to 2^31 = 2.1B) can hold any NUMBER(10, 0) value
+                // (up to 10^9 = 1B).
+                internalType = SqlStatement.Type.INT;
+            } else if (scale == 0 && precision <= 19) {
+                // An int (up to 2^31 = 2.1B) can hold any NUMBER(10, 0) value
+                // (up to 10^9 = 1B).
+                internalType = SqlStatement.Type.LONG;
+            } else {
+                internalType = SqlStatement.Type.DOUBLE;
+            }
+        } else {
+            internalType = VERTICA_TYPE_MAP.get(columnType);
+            if (internalType == null) {
+                internalType = SqlStatement.Type.OBJECT;
+            }
+        }
+        logTypeInfo(metaData, columnIndex, internalType);
+        return internalType;
     }
 
+    @Override
+    public boolean supportsMultiValueInExpr() {
+        return true;
+    }
 }
 
 // End VerticaDialect.java
