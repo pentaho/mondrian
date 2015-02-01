bfbe04537da5ba1a391382b15322efe3cbd41830
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2015-02-01 15:11:29 +0300

    [MONDRIAN-2256] - YTD/QTD/MTD/etc. do not work with the CurrentDateMember function

--- src/main/mondrian/udf/CurrentDateMemberUdf.java
+++ src/main/mondrian/udf/CurrentDateMemberUdf.java
@@ -1,12 +1,11 @@
 /*
-* This software is subject to the terms of the Eclipse Public License v1.0
-* Agreement, available at the following URL:
-* http://www.eclipse.org/legal/epl-v10.html.
-* You must accept the terms of that agreement to use this software.
-*
-* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (c) 2002-2015 Pentaho Corporation.  All rights reserved.
 */
-
 package mondrian.udf;
 
 import mondrian.olap.*;
@@ -116,7 +115,9 @@ public class CurrentDateMemberUdf implements UserDefinedFunction {
     }
 
     public Type getReturnType(Type[] parameterTypes) {
-        return MemberType.Unknown;
+        Dimension dim = parameterTypes[0].getDimension();
+        return (dim == null) ? MemberType.Unknown : MemberType
+            .forDimension(dim);
     }
 
     public Syntax getSyntax() {
