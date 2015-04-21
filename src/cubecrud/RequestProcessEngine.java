40aaea17569fbb82d990354c7f842598df08030a
shazra <sayan.mnit@gmail.com>
2015-04-21 23:12:37 +0200

    Cache invalidation for cubes and catalogs

--- src/cubecrud/RequestProcessEngine.java
+++ src/cubecrud/RequestProcessEngine.java
@@ -1,29 +1,21 @@
 package cubecrud;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.QueryParam;
-
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
 
-import javax.ws.rs.core.Context;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -40,20 +32,11 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
 import mondrian.olap.CacheControl;
-import mondrian.olap.Connection;
+import mondrian.olap.CacheControl.CellRegion;
 import mondrian.olap.Cube;
-import mondrian.olap.Dimension;
-import mondrian.olap.DriverManager;
-import mondrian.olap.MondrianServer;
-import mondrian.olap.Util.PropertyList;
-import mondrian.rolap.CacheControlImpl;
-import mondrian.rolap.RolapConnection;
-import mondrian.rolap.RolapConnectionProperties;
 import mondrian.rolap.RolapSchema;
-import mondrian.spi.CatalogLocator;
-import mondrian.spi.impl.CatalogLocatorImpl;
 
-import org.olap4j.OlapConnection;
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -63,9 +46,21 @@ import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-import org.apache.log4j.Logger;
 
 
+/**
+ *
+ * This class provides a REST API for cube management in the Mondrian  
+ * and also has functionality to invalidate the cache in Mondrian after
+ * data change in database.
+ * 
+ */
+
+/** 
+ *
+ * @author SHazra
+ * 
+ */
 
 
 @Path("/")
@@ -152,116 +147,71 @@ public class RequestProcessEngine {
 	@PUT
 	@Produces("application/xml")
 	@Consumes("text/plain")
-	public String invalidateCache(@PathParam("c") String catalogName) throws SQLException {
-    	// Assuming catalog name is unique in the datasource.xml
-		System.out.println("Inside invalidate cache with catalog name=" + catalogName);
-		RequestProcessEngine reqProcessObj = new RequestProcessEngine();
-		Document doc;
-		NodeList nodeList = null;
-		String datasourceName = "";
-		String connectionString = "";
-		String catalogLocator = "";
-		HashMap< String, String> datasourceMap = new HashMap<String, String>();
+	public String invalidateCacheCatalog(@PathParam("c") String catalogName) throws SQLException {
+    	boolean isCatalogFound = false;
 		try {
-//		  invalidate the whole cache
-		  StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        // get the connectStrings
-//        mondrian.olap.CacheControl cacheControl = OlapConnection.unwrap(mondrian.rolap.RolapConnection.class).getCacheControl(null);
-        String result = getCube(DATASOURCE_PATH, "", catalogName).replaceAll("&", "&#38;");
-        
-        System.out.println(" result = " + result);
-        ArrayList<Object> arrList = getNodeList(result, "/output/Catalog", false);
-        for (Object o: arrList)
-		{
-			if (o instanceof Document)
-			{
-				doc = (Document) o;
-			}
-			else if (o instanceof NodeList)
-			{
-				nodeList = (NodeList) o;
-			}
-		}
-        
-        if (nodeList != null && nodeList.getLength() > 0)
-    	{
-    		for (int x=0; x<nodeList.getLength();x++)
-    		{
-    			Node n = nodeList.item(x);
-    			System.out.println("node =" + n.getNodeName());
-    			NamedNodeMap attrs = n.getAttributes();
-    			String catalogNameFromXml = attrs.getNamedItem("name").getNodeValue();
-    			if (catalogName.equalsIgnoreCase(catalogNameFromXml))
-    			{
-    				if (attrs.getNamedItem("datasourceinfo") != null)
-                	{
-    					datasourceName = attrs.getNamedItem("datasourceinfo").getNodeValue();
-                		System.out.println(" Data source info is " + datasourceName);
-                		String dataSourceArr[] = datasourceName.split(";");
-                        for (String datasourceElem: dataSourceArr)
-                        {
-                        	if (datasourceElem != null && !"".equalsIgnoreCase(datasourceElem))
-                        	{
-                        		if (datasourceElem.contains("Jdbc=") || datasourceElem.contains("JDBC=")){
-                        			connectionString = datasourceElem.substring(4);
-                        		}
-                        		else if (datasourceElem.contains("Catalog=")){
-                        			catalogLocator = datasourceElem.substring(7);
-                        		}
-                        		
-                        	}
-                        	
-                        }
-                        break;
-                	}
-    			}
-    		}
-    	}
-        
-        System.out.println("The connect string is " + connectionString);
-        System.out.println("The catalog string is " + catalogLocator);
-
-//        CatalogLocator catalogLocatorObj = CatalogLocatorImpl.INSTANCE;
-//        catalogLocatorObj.locate(catalogLocator);
-//        Connection privateConnection = DriverManager.getConnection(datasourceName, catalogLocatorObj);
-//		//PrintWriter pw = null;
-//        System.out.println("The cache contents = ");
-//        privateConnection.getCacheControl(pw);
-//        pw.println();
-//        
-//        
-//        
-//        Connection privateConnection1 = DriverManager.getConnection(datasourceName, null);
-//		//PrintWriter pw = null;
-//        System.out.println("The cache contents2 = ");
-//        privateConnection1.getCacheControl(pw);
-//        pw.println();
-        
-        for (RolapSchema schema : RolapSchema.getRolapSchemas()) {
-        	System.out.println("schema name= " + schema.getName());
-        	Cube [] cubeArr = schema.getCubes();
-          for (Cube cube : cubeArr)
-          {
-        	  System.out.println("cube name =" + cube.getName());
-          }
+		System.out.println("Inside invalidate cache with catalog name=" + catalogName);
+		for (RolapSchema schema : RolapSchema.getRolapSchemas()) {
             if (schema.getName().equals(catalogName)) {
-            	System.out.println("schema is same as catalog and is flushing the schema");
+            	isCatalogFound = true;
+            	LOGGER.debug("schema is same as catalog and is flushing the schema");
                 schema.getInternalConnection().getCacheControl(null)
                     .flushSchemaCache();                 
             }
         }
-        
-//        mondrian.olap.CacheControl cacheControl = privateConnection1.getCacheControl(null);
-//        mondrian.olap.Schema schema = privateConnection1.getSchema();
+		if (!isCatalogFound)
+		{
+			return "<output>Catalog " + catalogName + " was not found </output>";
+		}
+
      
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			LOGGER.error("Error in RequestProcessEngine | invalidateCache " + e.getMessage());
+			e.printStackTrace();			
+			return "<output>Error in clearing cache for catalog " + catalogName + " | " + e.getMessage() 
+					+ "</output>";
 		}
-		String result = "<output>Catalog" + catalogName + " cache is cleared</output>";
-		return result;
+		return "<output>Cache clearance for Catalog " + catalogName + " is successful</output>";
 	}
+	
+	@Path("/invalidatecache/cube/{c}")
+	@PUT
+	@Produces("application/xml")
+	@Consumes("text/plain")
+	public String invalidateCacheCube(@PathParam("c") String cubeName) throws SQLException {
+    	// Warning: Leads to inconsistency in the view if the atomicity of the flush and DB update
+		// are not taken in to account
+		boolean isCubePresent = false;
+		try {		
+		for (RolapSchema schema : RolapSchema.getRolapSchemas()) {
+        	Cube [] cubeArr = schema.getCubes();
+        	CacheControl cacheControlObj = schema.getInternalConnection().getCacheControl(null);
+          for (Cube cube : cubeArr)
+          {
+        	  if (cube.getName().equals(cubeName)) {
+        		isCubePresent = true;
+              	LOGGER.debug("cube entered is same as cube and is flushing the cube");                
+                cacheControlObj.flush(cacheControlObj.createMeasuresRegion(cube));                
+              }
+          }            
+        }
+		if (!isCubePresent)
+		{
+			return "<output>Cube " + cubeName + " was not found </output>";
+		}
+
+     
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			LOGGER.error("Error in RequestProcessEngine | invalidateCache " + e.getMessage());
+			e.printStackTrace();			
+			return "<output>Error in clearing cache for cube " + cubeName + " | " + e.getMessage() 
+					+ "</output>";
+		}
+		return "<output>Cache clearance for Cube " + cubeName + " is successful</output>";
+	}
+	
 	
 	private static String parseCubeXml(String fileName, String cubeName){
         StreamResult result = null;
