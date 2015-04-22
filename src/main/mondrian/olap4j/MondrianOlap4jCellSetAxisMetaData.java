cbc4e8818b6487bd5e60f52dac8d2f0e5f089456
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2015-04-22 14:06:22 +0300

    [MONDRIAN-2302] - formatting, Checkstyle

--- src/main/mondrian/olap4j/MondrianOlap4jCellSetAxisMetaData.java
+++ src/main/mondrian/olap4j/MondrianOlap4jCellSetAxisMetaData.java
@@ -1,12 +1,13 @@
 /*
-* This software is subject to the terms of the Eclipse Public License v1.0
-* Agreement, available at the following URL:
-* http://www.eclipse.org/legal/epl-v10.html.
-* You must accept the terms of that agreement to use this software.
-*
-* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (C) 2003-2005 Julian Hyde
+// Copyright (C) 2005-2015 Pentaho
+// All Rights Reserved.
 */
-
 package mondrian.olap4j;
 
 import mondrian.mdx.LevelExpr;
