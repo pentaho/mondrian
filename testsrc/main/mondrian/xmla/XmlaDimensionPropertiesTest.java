8d9f272fc0b8a2b322e7d174b5a5ae7f6664b478
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2015-05-12 18:47:31 +0300

    MONDRIAN-2302 - Revorked Tests.

--- testsrc/main/mondrian/xmla/XmlaDimensionPropertiesTest.java
+++ testsrc/main/mondrian/xmla/XmlaDimensionPropertiesTest.java
@@ -32,47 +32,10 @@ public class XmlaDimensionPropertiesTest extends XmlaBaseTestCase {
     }
 
     private void executeTest() throws Exception {
-        final String dimensions = defineDimensions();
-        TestContext context = TestContext.instance().createSubstitutingCube(
-            "HR", dimensions);
+        TestContext context = getTestContext().withCube("HR");
         String requestType = "EXECUTE";
         Properties props = getDefaultRequestProperties(requestType);
         doTest(requestType, props, context);
-    }
-
-    private String defineDimensions() {
-        String result =
-            "<Dimension name=\"Store\" foreignKey=\"employee_id\" >\r\n"
-            + "  <Hierarchy hasAll=\"true\" primaryKey=\"employee_id\"\r\n"
-            + "      primaryKeyTable=\"employee\">\r\n"
-            + "    <Join leftKey=\"store_id\" rightKey=\"store_id\">\r\n"
-            + "      <Table name=\"employee\"/>\r\n"
-            + "      <Table name=\"store\"/>\r\n"
-            + "    </Join>\r\n"
-            + "    <Level name=\"Store Country\" table=\"store\" column=\"store_country\"\r\n"
-            + "        uniqueMembers=\"true\"/>\r\n"
-            + "    <Level name=\"Store State\" table=\"store\" column=\"store_state\"\r\n"
-            + "        uniqueMembers=\"true\"/>\r\n"
-            + "    <Level name=\"Store City\" table=\"store\" column=\"store_city\"\r\n"
-            + "        uniqueMembers=\"false\"/>\r\n"
-            + "    <Level name=\"Store Name\" table=\"store\" column=\"store_name\"\r\n"
-            + "        uniqueMembers=\"true\">\r\n"
-            + "      <Property name=\"Store Manager\" column=\"store_manager\"/>\r\n"
-            + "    </Level>\r\n"
-            + "  </Hierarchy>\r\n"
-            + "</Dimension>\r\n"
-            + "<Dimension name=\"Store Type\" foreignKey=\"employee_id\">\r\n"
-            + "  <Hierarchy hasAll=\"true\" primaryKeyTable=\"employee\" primaryKey=\"employee_id\">\r\n"
-            + "    <Join leftKey=\"store_id\" rightKey=\"store_id\">\r\n"
-            + "      <Table name=\"employee\"/>\r\n"
-            + "      <Table name=\"store\"/>\r\n"
-            + "    </Join>\r\n"
-            + "          <Level name=\"Store Type\" table=\"store\" column=\"store_type\" uniqueMembers=\"true\">\r\n"
-            + "      <Property name=\"Store Manager\" column=\"store_manager\"/>\r\n"
-            + "    </Level>\r\n"
-            + "  </Hierarchy>\r\n"
-            + "</Dimension>\r\n";
-        return result;
     }
 
     @Override
