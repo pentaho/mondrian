26fde8aa4d28133f760c52eac23d3fcbea2676c0
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-09-30 04:53:48 +0400

    [SP-2167] - Backport of MONDRIAN-2394 - Mondrian TopCount returns only non empty tuples when native.topcount is enabled (6.0 Suite) Backport [MONDRIAN-1272] - Native Top Count cannot handle empty members: - adopt changes from dbdc2bfe1d05b8f11a82f3d3532adce3132d0d7c   - the idea is to complete output data with result of one new query, where existing rows are filtered - add tests (cherry picked from commit c02450c)

--- testsrc/main/mondrian/rolap/RolapNativeTopCountTest.java
+++ testsrc/main/mondrian/rolap/RolapNativeTopCountTest.java
@@ -4,15 +4,18 @@
 // http://www.eclipse.org/legal/epl-v10.html.
 // You must accept the terms of that agreement to use this software.
 //
-// Copyright (C) 2015-2015 Pentaho ant others
+// Copyright (C) 2015-2015 Pentaho and others
 // All Rights Reserved.
 */
 package mondrian.rolap;
 
 import mondrian.test.TestContext;
 
+import static mondrian.rolap.RolapNativeTopCountTestCases.*;
+
 /**
  * @author Andrey Khayrutdinov
+ * @see RolapNativeTopCountTestCases
  */
 public class RolapNativeTopCountTest extends BatchTestCase {
 
@@ -22,102 +25,76 @@ public class RolapNativeTopCountTest extends BatchTestCase {
     }
 
     public void testTopCount_ImplicitCountMeasure() throws Exception {
-        final String query = ""
-            + "SELECT [Measures].[Fact Count] ON COLUMNS, "
-            + "TOPCOUNT([Store Type].[All Store Types].Children, 3, [Measures].[Fact Count]) ON ROWS "
-            + "FROM [Store]";
-
-        final String expectedResult = ""
-            + "Axis #0:\n"
-            + "{}\n"
-            + "Axis #1:\n"
-            + "{[Measures].[Fact Count]}\n"
-            + "Axis #2:\n"
-            + "{[Store Type].[Supermarket]}\n"
-            + "{[Store Type].[Deluxe Supermarket]}\n"
-            + "{[Store Type].[Mid-Size Grocery]}\n"
-            + "Row #0: 8\n"
-            + "Row #1: 6\n"
-            + "Row #2: 4\n";
-
-        TestContext.instance().assertQueryReturns(query, expectedResult);
+        assertQueryReturns(
+            IMPLICIT_COUNT_MEASURE_QUERY, IMPLICIT_COUNT_MEASURE_RESULT);
     }
 
     public void testTopCount_CountMeasure() throws Exception {
-        final String cube = ""
-            + "  <Cube name=\"StoreWithCountM\" visible=\"true\" cache=\"true\" enabled=\"true\">\n"
-            + "    <Table name=\"store\">\n"
-            + "    </Table>\n"
-            + "    <Dimension visible=\"true\" highCardinality=\"false\" name=\"Store Type\">\n"
-            + "      <Hierarchy visible=\"true\" hasAll=\"true\">\n"
-            + "        <Level name=\"Store Type\" visible=\"true\" column=\"store_type\" type=\"String\" uniqueMembers=\"true\" levelType=\"Regular\" hideMemberIf=\"Never\">\n"
-            + "        </Level>\n"
-            + "      </Hierarchy>\n"
-            + "    </Dimension>\n"
-            + "    <DimensionUsage source=\"Store\" name=\"Store\" visible=\"true\" highCardinality=\"false\">\n"
-            + "    </DimensionUsage>\n"
-            + "    <Dimension visible=\"true\" highCardinality=\"false\" name=\"Has coffee bar\">\n"
-            + "      <Hierarchy visible=\"true\" hasAll=\"true\">\n"
-            + "        <Level name=\"Has coffee bar\" visible=\"true\" column=\"coffee_bar\" type=\"Boolean\" uniqueMembers=\"true\" levelType=\"Regular\" hideMemberIf=\"Never\">\n"
-            + "        </Level>\n"
-            + "      </Hierarchy>\n"
-            + "    </Dimension>\n"
-            + "    <Measure name=\"Store Sqft\" column=\"store_sqft\" formatString=\"#,###\" aggregator=\"sum\">\n"
-            + "    </Measure>\n"
-            + "    <Measure name=\"Grocery Sqft\" column=\"grocery_sqft\" formatString=\"#,###\" aggregator=\"sum\">\n"
-            + "    </Measure>\n"
-            + "    <Measure name=\"CountM\" column=\"store_id\" formatString=\"Standard\" aggregator=\"count\" visible=\"true\">\n"
-            + "    </Measure>\n"
-            + "  </Cube>";
-
         final String schema = TestContext.instance()
-            .getSchema(null, cube, null, null, null, null);
+                .getSchema(
+                    null, CUSTOM_COUNT_MEASURE_CUBE,
+                    null, null, null, null);
 
         TestContext ctx = TestContext.instance()
-            .withSchema(schema)
-            .withCube("StoreWithCountM");
+                .withSchema(schema)
+                .withCube(CUSTOM_COUNT_MEASURE_CUBE_NAME);
 
-        final String query = ""
-            + "SELECT [Measures].[CountM] ON COLUMNS, "
-            + "TOPCOUNT([Store Type].[All Store Types].Children, 3, [Measures].[CountM]) ON ROWS "
-            + "FROM [StoreWithCountM]";
-
-        final String expectedResult = ""
-            + "Axis #0:\n"
-            + "{}\n"
-            + "Axis #1:\n"
-            + "{[Measures].[CountM]}\n"
-            + "Axis #2:\n"
-            + "{[Store Type].[Supermarket]}\n"
-            + "{[Store Type].[Deluxe Supermarket]}\n"
-            + "{[Store Type].[Mid-Size Grocery]}\n"
-            + "Row #0: 8\n"
-            + "Row #1: 6\n"
-            + "Row #2: 4\n";
-
-        ctx.assertQueryReturns(query, expectedResult);
+        ctx.assertQueryReturns(
+            CUSTOM_COUNT_MEASURE_QUERY, CUSTOM_COUNT_MEASURE_RESULT);
     }
 
     public void testTopCount_SumMeasure() throws Exception {
-        final String query = ""
-            + "SELECT [Measures].[Store Sqft] ON COLUMNS, "
-            + "TOPCOUNT([Store Type].[All Store Types].Children, 3, [Measures].[Store Sqft]) ON ROWS "
-            + "FROM [Store]";
+        assertQueryReturns(SUM_MEASURE_QUERY, SUM_MEASURE_RESULT);
+    }
 
-        final String expectedResult = ""
-            + "Axis #0:\n"
-            + "{}\n"
-            + "Axis #1:\n"
-            + "{[Measures].[Store Sqft]}\n"
-            + "Axis #2:\n"
-            + "{[Store Type].[Supermarket]}\n"
-            + "{[Store Type].[Deluxe Supermarket]}\n"
-            + "{[Store Type].[Mid-Size Grocery]}\n"
-            + "Row #0: 193,480\n"
-            + "Row #1: 146,045\n"
-            + "Row #2: 109,343\n";
 
-        TestContext.instance().assertQueryReturns(query, expectedResult);
+    public void testEmptyCellsAreShown_Countries() {
+        assertQueryReturns(
+            EMPTY_CELLS_ARE_SHOWN_COUNTRIES_QUERY,
+            EMPTY_CELLS_ARE_SHOWN_COUNTRIES_RESULT);
+    }
+
+    public void testEmptyCellsAreShown_States() {
+        assertQueryReturns(
+            EMPTY_CELLS_ARE_SHOWN_STATES_QUERY,
+            EMPTY_CELLS_ARE_SHOWN_STATES_RESULT);
+    }
+
+    public void testEmptyCellsAreShown_ButNoMoreThanReallyExist() {
+        assertQueryReturns(
+            EMPTY_CELLS_ARE_SHOWN_NOT_MORE_THAN_EXIST_QUERY,
+            EMPTY_CELLS_ARE_SHOWN_NOT_MORE_THAN_EXIST_RESULT);
+    }
+
+    public void testEmptyCellsAreHidden_WhenNonEmptyIsDeclaredExplicitly() {
+        assertQueryReturns(
+            EMPTY_CELLS_ARE_HIDDEN_WHEN_NON_EMPTY_QUERY,
+            EMPTY_CELLS_ARE_HIDDEN_WHEN_NON_EMPTY_RESULT);
+    }
+
+
+    public void testRoleRestrictionWorks_ForRowWithData() throws Exception {
+        TestContext ctx = TestContext.instance()
+            .create(
+                null, null, null, null, null,
+                ROLE_RESTRICTION_WORKS_WA_ROLE_DEF)
+            .withRole(ROLE_RESTRICTION_WORKS_WA_ROLE_NAME);
+
+        ctx.assertQueryReturns(
+            ROLE_RESTRICTION_WORKS_WA_QUERY,
+            ROLE_RESTRICTION_WORKS_WA_RESULT);
+    }
+
+    public void testRoleRestrictionWorks_ForRowWithOutData() throws Exception {
+        TestContext ctx = TestContext.instance()
+            .create(
+                null, null, null, null, null,
+                ROLE_RESTRICTION_WORKS_DF_ROLE_DEF)
+            .withRole(ROLE_RESTRICTION_WORKS_DF_ROLE_NAME);
+
+        ctx.assertQueryReturns(
+            ROLE_RESTRICTION_WORKS_DF_QUERY,
+            ROLE_RESTRICTION_WORKS_DF_RESULT);
     }
 }
 
---
+++ testsrc/main/mondrian/rolap/RolapNativeTopCountVersusNonNativeTest.java
@@ -0,0 +1,112 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (C) 2015-2015 Pentaho and others
+// All Rights Reserved.
+*/
+package mondrian.rolap;
+
+import mondrian.test.TestContext;
+
+import static mondrian.rolap.RolapNativeTopCountTestCases.*;
+
+/**
+ * @author Andrey Khayrutdinov
+ */
+public class RolapNativeTopCountVersusNonNativeTest extends BatchTestCase {
+
+    private void assertResultsAreEqual(String testCase, String query) {
+        assertResultsAreEqual(testCase, query, getTestContext());
+    }
+
+    private void assertResultsAreEqual(
+        String testCase,
+        String query,
+        TestContext ctx)
+    {
+        String message = String.format(
+            "[%s]: native and non-native results of the query differ. The query:\n\t\t%s",
+            testCase,
+            query);
+        verifySameNativeAndNot(query, message, ctx);
+    }
+
+
+    public void testTopCount_ImplicitCountMeasure() throws Exception {
+        assertResultsAreEqual(
+            "Implicit Count Measure", IMPLICIT_COUNT_MEASURE_QUERY);
+    }
+
+    public void testTopCount_SumMeasure() throws Exception {
+        assertResultsAreEqual(
+            "Sum Measure", SUM_MEASURE_QUERY);
+    }
+
+    public void testTopCount_CountMeasure() throws Exception {
+        final String schema = TestContext.instance()
+            .getSchema(null, CUSTOM_COUNT_MEASURE_CUBE, null, null, null, null);
+
+        TestContext ctx = TestContext.instance()
+            .withSchema(schema)
+            .withCube(CUSTOM_COUNT_MEASURE_CUBE_NAME);
+
+        assertResultsAreEqual(
+            "Custom Count Measure", CUSTOM_COUNT_MEASURE_QUERY, ctx);
+    }
+
+
+    public void testEmptyCellsAreShown_Countries() throws Exception {
+        assertResultsAreEqual(
+            "Empty Cells Are Shown - Countries",
+            EMPTY_CELLS_ARE_SHOWN_COUNTRIES_QUERY);
+    }
+
+    public void testEmptyCellsAreShown_States() throws Exception {
+        assertResultsAreEqual(
+            "Empty Cells Are Shown - States",
+            EMPTY_CELLS_ARE_SHOWN_STATES_QUERY);
+    }
+
+    public void testEmptyCellsAreShown_ButNoMoreThanReallyExist() {
+        assertResultsAreEqual(
+            "Empty Cells Are Shown - But no more than really exist",
+            EMPTY_CELLS_ARE_SHOWN_NOT_MORE_THAN_EXIST_QUERY);
+    }
+
+    public void testEmptyCellsAreHidden_WhenNonEmptyIsDeclaredExplicitly() {
+        assertResultsAreEqual(
+            "Empty Cells Are Hidden - When NON EMPTY is declared explicitly",
+            EMPTY_CELLS_ARE_HIDDEN_WHEN_NON_EMPTY_QUERY);
+    }
+
+
+    public void testRoleRestrictionWorks_ForRowWithData() {
+        TestContext ctx = TestContext.instance()
+            .create(
+                null, null, null, null, null,
+                ROLE_RESTRICTION_WORKS_WA_ROLE_DEF)
+            .withRole(ROLE_RESTRICTION_WORKS_WA_ROLE_NAME);
+
+        assertResultsAreEqual(
+            "Role restriction works - For WA state",
+            ROLE_RESTRICTION_WORKS_WA_QUERY, ctx);
+    }
+
+    public void testRoleRestrictionWorks_ForRowWithOutData() {
+        TestContext ctx = TestContext.instance()
+            .create(
+                null, null, null, null, null,
+                ROLE_RESTRICTION_WORKS_DF_ROLE_DEF)
+            .withRole(ROLE_RESTRICTION_WORKS_DF_ROLE_NAME);
+
+        assertResultsAreEqual(
+            "Role restriction works - For DF state",
+            ROLE_RESTRICTION_WORKS_DF_QUERY, ctx);
+    }
+
+}
+
+// End RolapNativeTopCountVersusNonNativeTest.java
