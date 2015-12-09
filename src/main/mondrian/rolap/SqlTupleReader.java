a3d1a8f63d55bdf56383e4fe2b529917b217a86a
Marc Batchelor <mbatchelor@pentaho.com>
2015-12-09 15:35:42 -0500

    [MONDRIAN-2161] check query cancel while reading sql results

--- src/main/mondrian/rolap/SqlTupleReader.java
+++ src/main/mondrian/rolap/SqlTupleReader.java
@@ -6,7 +6,7 @@
 //
 // Copyright (C) 2004-2005 TONBELLER AG
 // Copyright (C) 2005-2005 Julian Hyde
-// Copyright (C) 2005-2013 Pentaho and others
+// Copyright (C) 2005-2014 Pentaho and others
 // All Rights Reserved.
 */
 package mondrian.rolap;
@@ -29,7 +29,6 @@ import org.apache.log4j.Logger;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.*;
 
 import javax.sql.DataSource;
@@ -416,6 +415,8 @@ public class SqlTupleReader implements TupleReader {
             }
 
             int limit = MondrianProperties.instance().ResultLimit.get();
+            final int checkCancelPeriod =
+                MondrianProperties.instance().CancelPhaseInterval.get();
             int fetchCount = 0;
 
             // determine how many enum targets we have
@@ -440,6 +441,13 @@ public class SqlTupleReader implements TupleReader {
                     // result limit exceeded, throw an exception
                     throw MondrianResource.instance().MemberFetchLimitExceeded
                         .ex((long) limit);
+                }
+
+                // Check if the MDX query was canceled.
+                if (checkCancelPeriod > 0
+                    && stmt.rowCount % checkCancelPeriod == 0)
+                {
+                    Locus.peek().execution.checkCancelOrTimeout();
                 }
 
                 if (enumTargetCount == 0) {
