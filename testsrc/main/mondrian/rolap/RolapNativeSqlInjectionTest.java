232f75e45d3a133c58f23e44daa0579f08adfa10
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-12-14 11:31:52 +0400

    [MONDRIAN-2436] - SQL Injection through MDX Filter - prohibit invalid decimal literals in NumberSqlCompiler, throw exception in such cases - add tests

---
+++ testsrc/main/mondrian/rolap/RolapNativeSqlInjectionTest.java
@@ -0,0 +1,43 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (c) 2015-2015 Pentaho Corporation.
+// All Rights Reserved.
+*/
+package mondrian.rolap;
+
+import mondrian.olap.MondrianException;
+import mondrian.test.FoodMartTestCase;
+import mondrian.test.TestContext;
+
+/**
+ * @author Andrey Khayrutdinov
+ */
+public class RolapNativeSqlInjectionTest extends FoodMartTestCase {
+
+    public void testMondrian2436() {
+        String mdxQuery = ""
+            + "select {[Measures].[Store Sales]} on columns, "
+            + "filter([Customers].[Name].Members, (([Measures].[Store Sales]) > '(select 1000)')) on rows "
+            + "from [Sales]";
+
+        TestContext context = getTestContext();
+        try {
+            context.executeQuery(mdxQuery);
+        } catch (MondrianException e) {
+            assertNotNull(
+                "MondrianEvaluationException is expected on invalid filter condition",
+                e.getCause());
+            assertEquals(
+                "Expected to get decimal, but got (select 1000)",
+                e.getCause().getMessage());
+            return;
+        }
+        fail("[Store Sales] filtering should not work for non-valid decimals");
+    }
+}
+
+// End RolapNativeSqlInjectionTest.java
---
+++ testsrc/main/mondrian/rolap/RolapNativeSql_NumberSqlCompiler_Test.java
@@ -0,0 +1,75 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (c) 2015-2015 Pentaho Corporation.
+// All Rights Reserved.
+*/
+package mondrian.rolap;
+
+import mondrian.calc.DummyExp;
+import mondrian.olap.Exp;
+import mondrian.olap.Literal;
+import mondrian.olap.type.NullType;
+import mondrian.rolap.sql.SqlQuery;
+import mondrian.spi.Dialect;
+
+import junit.framework.TestCase;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Andrey Khayrutdinov
+ */
+public class RolapNativeSql_NumberSqlCompiler_Test extends TestCase {
+
+    private RolapNativeSql.NumberSqlCompiler compiler;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        Dialect dialect = mock(Dialect.class);
+        when(dialect.getDatabaseProduct())
+            .thenReturn(Dialect.DatabaseProduct.MYSQL);
+
+        SqlQuery query = mock(SqlQuery.class);
+        when(query.getDialect()).thenReturn(dialect);
+
+        RolapNativeSql sql = new RolapNativeSql(query, null, null, null);
+        compiler = sql.new NumberSqlCompiler();
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        compiler = null;
+        super.tearDown();
+    }
+
+    public void testRejectsNonLiteral() {
+        Exp exp = new DummyExp(new NullType());
+        assertNull(compiler.compile(exp));
+    }
+
+    public void testAcceptsNumeric() {
+        Exp exp = Literal.create(BigDecimal.ONE);
+        assertNotNull(compiler.compile(exp));
+    }
+
+    public void testAcceptsString() {
+        Exp exp = Literal.createString("1");
+        assertNotNull(compiler.compile(exp));
+    }
+
+    public void testRejectsStrings_ThatCannotBeParsedAsDouble() {
+        Exp exp = Literal.createString("(select 100)");
+        assertNull(compiler.compile(exp));
+    }
+}
+
+// End RolapNativeSql_NumberSqlCompiler_Test.java
