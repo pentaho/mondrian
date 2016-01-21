4174aef02d1a9dd1567ced7b871982b98a59dfe5
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2016-01-21 18:53:42 +0300

    [SP-2458] - Backport of MONDRIAN-2279 - Test

--- testsrc/main/mondrian/rolap/CancellationTest.java
+++ testsrc/main/mondrian/rolap/CancellationTest.java
@@ -4,7 +4,7 @@
 // http://www.eclipse.org/legal/epl-v10.html.
 // You must accept the terms of that agreement to use this software.
 //
-// Copyright (c) 2002-2015 Pentaho Corporation..  All rights reserved.
+// Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
 */
 package mondrian.rolap;
 
@@ -15,6 +15,7 @@ import mondrian.olap.*;
 import mondrian.olap.fun.CrossJoinFunDef;
 import mondrian.olap.fun.CrossJoinTest;
 import mondrian.server.Execution;
+import mondrian.server.Locus;
 import mondrian.test.FoodMartTestCase;
 
 import static org.mockito.Mockito.*;
@@ -28,8 +29,8 @@ public class CancellationTest extends FoodMartTestCase {
         propSaver.set(propSaver.properties.CheckCancelOrTimeoutInterval, 1);
         CrossJoinFunDefTester crossJoinFunDef =
                 new CrossJoinFunDefTester(new CrossJoinTest.NullFunDef());
-        Result result = executeQuery(
-            "select store.[store name].members on 0 from sales");
+        Result result =
+            executeQuery("select store.[store name].members on 0 from sales");
         Evaluator eval = ((RolapResult) result).getEvaluator(new int[]{0});
         TupleList list = new UnaryTupleList();
         for (Position pos : result.getAxes()[0].getPositions()) {
@@ -42,6 +43,59 @@ public class CancellationTest extends FoodMartTestCase {
         // for each tuple since phase interval is 1
         verify(exec, times(list.size())).checkCancelOrTimeout();
     }
+
+    public void testMutableCrossJoinCancellation() throws MondrianException {
+        // tests that cancellation/timeout is checked in
+        // CrossJoinFunDef.mutableCrossJoin
+        propSaver.set(propSaver.properties.CheckCancelOrTimeoutInterval, 1);
+
+        RolapCube salesCube = (RolapCube) cubeByName(
+            getTestContext().getConnection(),
+            "Sales");
+        SchemaReader salesCubeSchemaReader =
+            salesCube.getSchemaReader(
+                getTestContext().getConnection().getRole()).withLocus();
+
+        TupleList productMembers =
+            productMembersPotScrubbersPotsAndPans(salesCubeSchemaReader);
+
+        String selectGenders = "select Gender.members on 0 from sales";
+        Result genders = executeQuery(selectGenders);
+
+        Evaluator gendersEval =
+            ((RolapResult) genders).getEvaluator(new int[]{0});
+        TupleList genderMembers = new UnaryTupleList();
+        for (Position pos : genders.getAxes()[0].getPositions()) {
+            genderMembers.add(pos);
+        }
+
+        Execution execution =
+            spy(new Execution(genders.getQuery().getStatement(), 0));
+        TupleList mutableCrossJoinResult =
+            mutableCrossJoin(productMembers, genderMembers, execution);
+
+        gendersEval.getQuery().getStatement().start(execution);
+
+        // checkCancelOrTimeout should be called once
+        // for each tuple from mutableCrossJoin since phase interval is 1
+        // plus once for each productMembers item
+        // since it gets through SqlStatement.execute
+        int expectedCallsQuantity =
+            mutableCrossJoinResult.size() + productMembers.size();
+        verify(execution, times(expectedCallsQuantity)).checkCancelOrTimeout();
+    }
+
+    private TupleList mutableCrossJoin(
+        final TupleList list1, final TupleList list2, final Execution execution)
+        {
+            return Locus.execute(
+                execution, "CancellationTest",
+                new Locus.Action<TupleList>() {
+                    public TupleList execute() {
+                        return CrossJoinFunDef.mutableCrossJoin(list1, list2);
+                    }
+                });
+        }
 
     public class CrossJoinFunDefTester extends CrossJoinFunDef {
         public CrossJoinFunDefTester(FunDef dummyFunDef) {
