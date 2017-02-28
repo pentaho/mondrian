0c38d93468e0feac40c0164e3da0f2b0d1103047
Kurtis Walker <kwalker@pentaho.com>
2017-02-28 16:34:11 -0500

    [TEST] - changing embedded mysql database to use memory storage engine

--- mondrian/src/it/resources/sql/create-foodmart.sql
+++ mondrian/src/it/resources/sql/create-foodmart.sql
@@ -1,3 +1,6 @@
 drop database if exists foodmart;
 create database foodmart;
-SET @@global.time_zone='-05:00'
+SET @@global.time_zone='-05:00';
+SET @@global.default_storage_engine=MEMORY;
+SET @@global.max_heap_table_size=4294967295;
+SET @@global.tmp_table_size=4294967295;
