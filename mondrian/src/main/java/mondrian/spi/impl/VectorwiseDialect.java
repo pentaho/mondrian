256c6a48edc4b40311301c0fdcf15432a83b1cf0
Yury Bakhmutski <YURY_BAKHMUTSKI@epam.com>
2017-05-26 18:51:02 +0300

    [MONDRIAN-2253] - Pentaho analyzer is throwing error when multiple shared dimensions are used in a virtual cube - Vectorwise DB

--- mondrian/src/main/java/mondrian/spi/impl/VectorwiseDialect.java
+++ mondrian/src/main/java/mondrian/spi/impl/VectorwiseDialect.java
@@ -1,13 +1,14 @@
 /*
-* This software is subject to the terms of the Eclipse Public License v1.0
-* Agreement, available at the following URL:
-* http://www.eclipse.org/legal/epl-v10.html.
-* You must accept the terms of that agreement to use this software.
-*
-* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
 */
-
 package mondrian.spi.impl;
+
+import org.apache.commons.lang.StringUtils;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -52,6 +53,22 @@ public class VectorwiseDialect extends IngresDialect {
     @Override
     public boolean requiresAliasForFromQuery() {
         return true;
+    }
+
+    @Override
+    protected String generateOrderByNulls(
+        String expr,
+        boolean ascending,
+        boolean collateNullsLast)
+    {
+        // expr may be a column number
+        if (StringUtils.isNumeric(expr)) {
+            expr = new Integer(expr).toString();
+        }
+        return super.generateOrderByNulls(
+            expr,
+            ascending,
+            collateNullsLast);
     }
 }
 
