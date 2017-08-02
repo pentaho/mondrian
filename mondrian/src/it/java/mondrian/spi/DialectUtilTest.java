ea0fa4dfff8a79ee7fa97fca55b16a28f5f578e3
Tatsiana Kasiankova <tatsiana_kasiankova@epam.com>
2017-08-02 23:41:55 +0300

    [SP-3653]: Backport of ANALYZER-3488 - Unable to do case insensitive search with multi-byte characters in filter dialog and using the Matches operator (7.1 Suite)
    This is the cherry-pick of the fix for [ANALYZER-3488]: Unable to do case insensitive search with multi-byte characters in filter dialog and using the Matches operator
    The problem:
    The fix: https://github.com/pentaho/pentaho-analyzer/commit/021a081d26fc51e13753ae5210073f38d5519b2d was not completed in full.
    It covers the DBs that are not supported native evalution of the Matches MDX function.
    In this case mondrian uses Java regex engine to evaluate matching.
    By default, case-insensitive matching assumes that only characters in the US-ASCII charset are being matched (Please see https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#UNICODE_CASE).
    So it was added (?u) flag to allow unicode-aware case folding for Java regex engine. Please see details in https://github.com/pentaho/pentaho-analyzer/commit/021a081d26fc51e13753ae5210073f38d5519b2d.
    
    But we have also BDs with implemented support of native evalution of the Matches MDX function (was introduced in MONDRIAN-944).
    None of them does not support (?u) flag. And moreove this flag doesn't need for them.
    So the fix is just to clean up the expression from this (?u) flag.
    Also added unit tests for all DB dialects with native support of regular expression.

---
+++ mondrian/src/it/java/mondrian/spi/DialectUtilTest.java
@@ -0,0 +1,37 @@
+/*
+// This software is subject to the terms of the Eclipse Public License v1.0
+// Agreement, available at the following URL:
+// http://www.eclipse.org/legal/epl-v10.html.
+// You must accept the terms of that agreement to use this software.
+//
+// Copyright (c) 2015-2017 Pentaho Corporation.
+// All rights reserved.
+ */
+package mondrian.spi;
+
+import junit.framework.TestCase;
+
+public class DialectUtilTest extends TestCase {
+
+  public void testCleanUnicodeAwareCaseFlag_InputNull() {
+    String inputExpression = null;
+    String cleaned = DialectUtil.cleanUnicodeAwareCaseFlag( inputExpression );
+    assertNull( cleaned );
+  }
+
+  public void testCleanUnicodeAwareCaseFlag_InputContainsFlag() {
+    String inputExpression = "(?i)|(?u).*ａ.*";
+    String expectedExpression = "(?i).*ａ.*";
+    String cleaned = DialectUtil.cleanUnicodeAwareCaseFlag( inputExpression );
+    assertEquals( expectedExpression, cleaned );
+  }
+
+  public void testCleanUnicodeAwareCaseFlag_InputNotContainsFlag() {
+    String inputExpression = "(?i).*ａ.*";
+    String expectedExpression = "(?i).*ａ.*";
+    String cleaned = DialectUtil.cleanUnicodeAwareCaseFlag( inputExpression );
+    assertEquals( expectedExpression, cleaned );
+  }
+
+}
+//End DialectUtilTest.java
