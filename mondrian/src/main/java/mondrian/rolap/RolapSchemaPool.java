bf7de143dc9f5d1f64031270b9259b09065c0dec
Bruno Costa <bruno.costa@hitachivantara.com>
2019-10-29 11:47:36 +0000

    [ANALYZER-3828] - Saving an inline model breaks localization

--- mondrian/src/main/java/mondrian/rolap/RolapSchemaPool.java
+++ mondrian/src/main/java/mondrian/rolap/RolapSchemaPool.java
@@ -5,7 +5,7 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 2001-2005 Julian Hyde and others
-// Copyright (C) 2005-2017 Hitachi Vantara and others
+// Copyright (C) 2005-2019 Hitachi Vantara and others
 // All Rights Reserved.
 */
 package mondrian.rolap;
