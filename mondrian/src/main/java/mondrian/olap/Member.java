e61c9cbce381ad3c507473853e61e1773b8978d6
Peter Rinehart <40572815+peterrinehart@users.noreply.github.com>
2020-04-06 09:40:00 -0400

    Mondrian 2664 redux (#1207)
    * [MONDRIAN-2664] Update CompoundSlicerRolapMember in order to allow it to
    be handled correctly in EXISTING function calculations.
    
    * [CLEANUP] Checkstyle fixes.

--- mondrian/src/main/java/mondrian/olap/Member.java
+++ mondrian/src/main/java/mondrian/olap/Member.java
@@ -5,7 +5,7 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 1999-2005 Julian Hyde
-// Copyright (C) 2005-2017 Hitachi Vantara and others
+// Copyright (C) 2005-2020 Hitachi Vantara and others
 // All Rights Reserved.
 */
 package mondrian.olap;
@@ -20,8 +20,7 @@ import java.util.List;
  * <code>[Measures].[Unit Sales]</code>.
  *
  * <p> Every member belongs to a {@link Level} of a {@link Hierarchy}. Members
- * except the root member have a parent, and members not at the leaf level
- * have one or more children.
+ * except the root member have a parent, and members not at the leaf level have one or more children.
  *
  * <p> Measures are a special kind of member. They belong to their own
  * dimension, <code>[Measures]</code>.
@@ -36,171 +35,177 @@ import java.util.List;
  */
 public interface Member extends OlapElement, Comparable, Annotated {
 
+  /**
+   * Returns this member's parent, or null (not the 'null member', as returned by {@link Hierarchy#getNullMember})
+   * if it
+   * has no parent.
+   *
+   * <p>In an access-control context, a member may have no <em>visible</em>
+   * parents, so use {@link SchemaReader#getMemberParent}.
+   */
+  Member getParentMember();
+
+  Level getLevel();
+
+  Hierarchy getHierarchy();
+
+  /**
+   * Returns name of parent member, or empty string (not null) if we are the root.
+   */
+  String getParentUniqueName();
+
+  /**
+   * Returns the type of member.
+   */
+  MemberType getMemberType();
+
+  /**
+   * @return True when the member is a leaf member, meaning it has no children
+   */
+  boolean isParentChildLeaf();
+
+  /**
+   * @return True when the member is part of a Parent-Child hierarchy and it is a physical member. In a Parent Child
+   * Hierarchy without a closure table, each member needs to be treated as calculated.  We need a way to distinguish
+   * between true calculated members and physical members that exist in the source data
+   */
+  boolean isParentChildPhysicalMember();
+
+  enum MemberType {
+    UNKNOWN,
+    REGULAR, // adMemberRegular
+    ALL,
+    MEASURE,
+    FORMULA,
     /**
-     * Returns this member's parent, or null (not the 'null member', as
-     * returned by {@link Hierarchy#getNullMember}) if it has no parent.
-     *
-     * <p>In an access-control context, a member may have no <em>visible</em>
-     * parents, so use {@link SchemaReader#getMemberParent}.
+     * This member is its hierarchy's NULL member (such as is returned by
+     * <code>[Gender]&#46;[All Gender]&#46;PrevMember</code>, for example).
      */
-    Member getParentMember();
+    NULL
+  }
 
-    Level getLevel();
+  /**
+   * Only allowable if the member is part of the <code>WITH</code> clause of a query.
+   */
+  void setName( String name );
 
-    Hierarchy getHierarchy();
+  /**
+   * Returns whether this is the 'all' member.
+   */
+  boolean isAll();
 
-    /**
-     * Returns name of parent member, or empty string (not null) if we are the
-     * root.
-     */
-    String getParentUniqueName();
+  /**
+   * Returns whether this is a member of the measures dimension.
+   */
+  boolean isMeasure();
 
-    /**
-     * Returns the type of member.
-     */
-    MemberType getMemberType();
+  /**
+   * Returns whether this is the 'null member'.
+   */
+  boolean isNull();
 
-    /**
-     * @return True when the member is a leaf member, meaning it has no children
-     */
-    boolean isParentChildLeaf();
+  /**
+   * Returns whether <code>member</code> is equal to, a child, or a descendent of this <code>Member</code>.
+   */
+  boolean isChildOrEqualTo( Member member );
 
-    /**
-     * @return True when the member is part of a Parent-Child hierarchy and it
-     * is a physical member. In a Parent Child Hierarchy without a closure
-     * table, each member needs to be treated as calculated.  We need a way to
-     * distinguish between true calculated members and physical members that
-     * exist in the source data
-     */
-    boolean isParentChildPhysicalMember();
+  /**
+   * Returns whether this member is computed using either a <code>with member</code> clause in an mdx query or a
+   * calculated member defined in cube.
+   */
+  boolean isCalculated();
 
-    enum MemberType {
-        UNKNOWN,
-        REGULAR, // adMemberRegular
-        ALL,
-        MEASURE,
-        FORMULA,
-        /**
-         * This member is its hierarchy's NULL member (such as is returned by
-         * <code>[Gender]&#46;[All Gender]&#46;PrevMember</code>, for example).
-         */
-        NULL
-    }
+  /**
+   * Returns whether this member should be evaluated within the Evaluator.
+   *
+   * <p>Normally {@link #isCalculated} and {@link #isEvaluated} should return
+   * the same value, but in situations where mondrian would like to treat the two concepts separately such in role based
+   * security, these values may differ.
+   *
+   * @return true if evaluated
+   */
+  boolean isEvaluated();
 
-    /**
-     * Only allowable if the member is part of the <code>WITH</code> clause of
-     * a query.
-     */
-    void setName(String name);
+  int getSolveOrder();
 
-    /** Returns whether this is the 'all' member. */
-    boolean isAll();
+  Exp getExpression();
 
-    /** Returns whether this is a member of the measures dimension. */
-    boolean isMeasure();
+  /**
+   * Returns a list of the ancestor members of this member.
+   *
+   * @deprecated Use {@link SchemaReader#getMemberAncestors(Member, java.util.List)}.
+   */
+  List<Member> getAncestorMembers();
 
-    /** Returns whether this is the 'null member'. */
-    boolean isNull();
+  /**
+   * Returns whether this member is computed from a {@code WITH MEMBER} clause in an MDX query.
+   */
+  boolean isCalculatedInQuery();
 
-    /**
-     * Returns whether <code>member</code> is equal to, a child, or a
-     * descendent of this <code>Member</code>.
-     */
-    boolean isChildOrEqualTo(Member member);
+  /**
+   * Returns the value of the property named <code>propertyName</code>. Name match is case-sensitive.
+   */
+  Object getPropertyValue( String propertyName );
 
-    /** Returns whether this member is computed using either a <code>with
-     * member</code> clause in an mdx query or a calculated member defined in
-     * cube. */
-    boolean isCalculated();
+  /**
+   * Returns the value of the property named <code>propertyName</code>, matching according to the required
+   * case-sensitivity.
+   */
+  Object getPropertyValue( String propertyName, boolean matchCase );
 
-    /**
-     * Returns whether this member should be evaluated within the Evaluator.
-     *
-     * <p>Normally {@link #isCalculated} and {@link #isEvaluated} should return
-     * the same value, but in situations where mondrian would like to treat the
-     * two concepts separately such in role based security, these values may
-     * differ.
-     *
-     * @return true if evaluated
-     */
-    boolean isEvaluated();
-    int getSolveOrder();
-    Exp getExpression();
+  /**
+   * Returns the formatted value of the property named
+   * <code>propertyName</code>.
+   */
+  String getPropertyFormattedValue( String propertyName );
 
-    /**
-     * Returns a list of the ancestor members of this member.
-     *
-     * @deprecated Use
-     *     {@link SchemaReader#getMemberAncestors(Member, java.util.List)}.
-     */
-    List<Member> getAncestorMembers();
+  /**
+   * Sets a property of this member to a given value.
+   */
+  void setProperty( String name, Object value );
 
-    /**
-     * Returns whether this member is computed from a {@code WITH MEMBER}
-     * clause in an MDX query.
-     */
-    boolean isCalculatedInQuery();
+  /**
+   * Returns the definitions of the properties this member may have.
+   */
+  Property[] getProperties();
 
-    /**
-     * Returns the value of the property named <code>propertyName</code>.
-     * Name match is case-sensitive.
-     */
-    Object getPropertyValue(String propertyName);
+  /**
+   * Returns the ordinal of the member.
+   */
+  int getOrdinal();
 
-    /**
-     * Returns the value of the property named <code>propertyName</code>,
-     * matching according to the required case-sensitivity.
-     */
-    Object getPropertyValue(String propertyName, boolean matchCase);
+  /**
+   * Returns the order key of the member (relative to its siblings); null if undefined or unavailable.
+   */
+  Comparable getOrderKey();
 
-    /**
-     * Returns the formatted value of the property named
-     * <code>propertyName</code>.
-     */
-    String getPropertyFormattedValue(String propertyName);
+  /**
+   * Returns whether this member is 'hidden', as per the rules which define a ragged hierarchy.
+   */
+  boolean isHidden();
 
-    /**
-     * Sets a property of this member to a given value.
-     */
-    void setProperty(String name, Object value);
+  /**
+   * returns the depth of this member, which is not the level's depth in case of parent child dimensions
+   *
+   * @return depth
+   */
+  int getDepth();
 
-    /**
-     * Returns the definitions of the properties this member may have.
-     */
-    Property[] getProperties();
+  /**
+   * Returns the system-generated data member that is associated with a nonleaf member of a dimension.
+   *
+   * <p>Returns this member if this member is a leaf member, or if the
+   * nonleaf member does not have an associated data member.</p>
+   */
+  Member getDataMember();
 
-    /**
-     * Returns the ordinal of the member.
-     */
-    int getOrdinal();
-
-    /**
-     * Returns the order key of the member (relative to its siblings);
-     * null if undefined or unavailable.
-     */
-    Comparable getOrderKey();
-
-    /**
-     * Returns whether this member is 'hidden', as per the rules which define
-     * a ragged hierarchy.
-     */
-    boolean isHidden();
-
-    /**
-     * returns the depth of this member, which is not the level's depth
-     *  in case of parent child dimensions
-     * @return depth
-     */
-    int getDepth();
-
-    /**
-     * Returns the system-generated data member that is associated with a
-     * nonleaf member of a dimension.
-     *
-     * <p>Returns this member if this member is a leaf member, or if the
-     * nonleaf member does not have an associated data member.</p>
-     */
-    Member getDataMember();
+  /**
+   * Returns true if this member is on the same hierarchy chain as <code>otherMember</code>.
+   *
+   * @param otherMember
+   * @return
+   */
+  boolean isOnSameHierarchyChain( Member otherMember );
 }
 
 // End Member.java
--- mondrian/src/main/java/mondrian/olap/MemberBase.java
+++ mondrian/src/main/java/mondrian/olap/MemberBase.java
@@ -5,11 +5,12 @@
 // You must accept the terms of that agreement to use this software.
 //
 // Copyright (C) 2001-2005 Julian Hyde
-// Copyright (C) 2005-2017 Hitachi Vantara and others
+// Copyright (C) 2005-2020 Hitachi Vantara and others
 // All Rights Reserved.
 */
 package mondrian.olap;
 
+import mondrian.olap.fun.FunUtil;
 import mondrian.resource.MondrianResource;
 import mondrian.spi.MemberFormatter;
 import mondrian.util.Bug;
@@ -24,270 +25,271 @@ import java.util.List;
  * @since 6 August, 2001
  */
 public abstract class MemberBase
-    extends OlapElementBase
-    implements Member
-{
+  extends OlapElementBase
+  implements Member {
 
-    protected Member parentMember;
-    protected final Level level;
-    protected String uniqueName;
+  protected Member parentMember;
+  protected final Level level;
+  protected String uniqueName;
 
-    /**
-     * Combines member type and other properties, such as whether the member
-     * is the 'all' or 'null' member of its hierarchy and whether it is a
-     * measure or is calculated, into an integer field.
-     *
-     * <p>The fields are:<ul>
-     * <li>bits 0, 1, 2 ({@link #FLAG_TYPE_MASK}) are member type;
-     * <li>bit 3 ({@link #FLAG_HIDDEN}) is set if the member is hidden;
-     * <li>bit 4 ({@link #FLAG_ALL}) is set if this is the all member of its
-     *     hierarchy;
-     * <li>bit 5 ({@link #FLAG_NULL}) is set if this is the null member of its
-     *     hierarchy;
-     * <li>bit 6 ({@link #FLAG_CALCULATED}) is set if this is a calculated
-     *     member.
-     * <li>bit 7 ({@link #FLAG_MEASURE}) is set if this is a measure.
-     * </ul>
-     *
-     * NOTE: jhyde, 2007/8/10. It is necessary to cache whether the member is
-     * 'all', 'calculated' or 'null' in the member's state, because these
-     * properties are used so often. If we used a virtual method call - say we
-     * made each subclass implement 'boolean isNull()' - it would be slower.
-     * We use one flags field rather than 4 boolean fields to save space.
-     */
-    protected final int flags;
+  /**
+   * Combines member type and other properties, such as whether the member is the 'all' or 'null' member of its
+   * hierarchy and whether it is a measure or is calculated, into an integer field.
+   *
+   * <p>The fields are:<ul>
+   * <li>bits 0, 1, 2 ({@link #FLAG_TYPE_MASK}) are member type;
+   * <li>bit 3 ({@link #FLAG_HIDDEN}) is set if the member is hidden;
+   * <li>bit 4 ({@link #FLAG_ALL}) is set if this is the all member of its
+   * hierarchy;
+   * <li>bit 5 ({@link #FLAG_NULL}) is set if this is the null member of its
+   * hierarchy;
+   * <li>bit 6 ({@link #FLAG_CALCULATED}) is set if this is a calculated
+   * member.
+   * <li>bit 7 ({@link #FLAG_MEASURE}) is set if this is a measure.
+   * </ul>
+   * <p>
+   * NOTE: jhyde, 2007/8/10. It is necessary to cache whether the member is 'all', 'calculated' or 'null' in the
+   * member's state, because these properties are used so often. If we used a virtual method call - say we made each
+   * subclass implement 'boolean isNull()' - it would be slower. We use one flags field rather than 4 boolean fields to
+   * save space.
+   */
+  protected final int flags;
 
-    private static final int FLAG_TYPE_MASK = 0x07;
-    private static final int FLAG_HIDDEN = 0x08;
-    private static final int FLAG_ALL = 0x10;
-    private static final int FLAG_NULL = 0x20;
-    private static final int FLAG_CALCULATED = 0x40;
-    private static final int FLAG_MEASURE = 0x80;
+  private static final int FLAG_TYPE_MASK = 0x07;
+  private static final int FLAG_HIDDEN = 0x08;
+  private static final int FLAG_ALL = 0x10;
+  private static final int FLAG_NULL = 0x20;
+  private static final int FLAG_CALCULATED = 0x40;
+  private static final int FLAG_MEASURE = 0x80;
 
-    /**
-     * Cached values of {@link mondrian.olap.Member.MemberType} enumeration.
-     * Without caching, get excessive calls to {@link Object#clone}.
-     */
-    private static final MemberType[] MEMBER_TYPE_VALUES = MemberType.values();
+  /**
+   * Cached values of {@link mondrian.olap.Member.MemberType} enumeration. Without caching, get excessive calls to
+   * {@link Object#clone}.
+   */
+  private static final MemberType[] MEMBER_TYPE_VALUES = MemberType.values();
 
-    protected MemberBase(
-        Member parentMember,
-        Level level,
-        MemberType memberType)
-    {
-        this.parentMember = parentMember;
-        this.level = level;
-        this.flags = memberType.ordinal()
-            | (memberType == MemberType.ALL ? FLAG_ALL : 0)
-            | (memberType == MemberType.NULL ? FLAG_NULL : 0)
-            | (computeCalculated(memberType) ? FLAG_CALCULATED : 0)
-            | (level.getHierarchy().getDimension().isMeasures()
-               ? FLAG_MEASURE
-               : 0);
+  protected MemberBase(
+    Member parentMember,
+    Level level,
+    MemberType memberType ) {
+    this.parentMember = parentMember;
+    this.level = level;
+    this.flags = memberType.ordinal()
+      | ( memberType == MemberType.ALL ? FLAG_ALL : 0 )
+      | ( memberType == MemberType.NULL ? FLAG_NULL : 0 )
+      | ( computeCalculated( memberType ) ? FLAG_CALCULATED : 0 )
+      | ( level.getHierarchy().getDimension().isMeasures()
+      ? FLAG_MEASURE
+      : 0 );
+  }
+
+  protected MemberBase() {
+    this.flags = 0;
+    this.level = null;
+  }
+
+  public String getQualifiedName() {
+    return MondrianResource.instance().MdxMemberName.str( getUniqueName() );
+  }
+
+  public abstract String getName();
+
+  public String getUniqueName() {
+    return uniqueName;
+  }
+
+  public String getCaption() {
+    // if there is a member formatter for the members level,
+    //  we will call this interface to provide the display string
+    MemberFormatter mf = getLevel().getMemberFormatter();
+    if ( mf != null ) {
+      return mf.formatMember( this );
     }
 
-    protected MemberBase() {
-        this.flags = 0;
-        this.level = null;
+    // fallback if formatter is null
+    final String caption = super.getCaption();
+    return ( caption != null )
+      ? caption
+      : getName();
+  }
+
+  public String getParentUniqueName() {
+    return parentMember == null
+      ? null
+      : parentMember.getUniqueName();
+  }
+
+  public Dimension getDimension() {
+    return level.getDimension();
+  }
+
+  public Hierarchy getHierarchy() {
+    return level.getHierarchy();
+  }
+
+  public Level getLevel() {
+    return level;
+  }
+
+  public MemberType getMemberType() {
+    return MEMBER_TYPE_VALUES[ flags & FLAG_TYPE_MASK ];
+  }
+
+  public String getDescription() {
+    return (String) getPropertyValue( Property.DESCRIPTION.name );
+  }
+
+  public boolean isMeasure() {
+    return ( flags & FLAG_MEASURE ) != 0;
+  }
+
+  public boolean isAll() {
+    return ( flags & FLAG_ALL ) != 0;
+  }
+
+  public boolean isNull() {
+    return ( flags & FLAG_NULL ) != 0;
+  }
+
+  public boolean isCalculated() {
+    return ( flags & FLAG_CALCULATED ) != 0;
+  }
+
+  public boolean isEvaluated() {
+    // should just call isCalculated(), but called in tight loops
+    // and too many subclass implementations for jit to inline properly?
+    return ( flags & FLAG_CALCULATED ) != 0;
+  }
+
+  public OlapElement lookupChild(
+    SchemaReader schemaReader,
+    Id.Segment childName,
+    MatchType matchType ) {
+    return schemaReader.lookupMemberChildByName(
+      this, childName, matchType );
+  }
+
+  // implement Member
+  public Member getParentMember() {
+    return parentMember;
+  }
+
+  // implement Member
+  public boolean isChildOrEqualTo( Member member ) {
+    // REVIEW: Using uniqueName to calculate ancestry seems inefficient,
+    //   because we can't afford to store every member's unique name, so
+    //   we want to compute it on the fly
+    assert !Bug.BugSegregateRolapCubeMemberFixed;
+    return ( member != null ) && isChildOrEqualTo( member.getUniqueName() );
+  }
+
+  /**
+   * Returns whether this <code>Member</code>'s unique name is equal to, a child of, or a descendent of a member whose
+   * unique name is
+   * <code>uniqueName</code>.
+   */
+  public boolean isChildOrEqualTo( String uniqueName ) {
+    if ( uniqueName == null ) {
+      return false;
     }
 
-    public String getQualifiedName() {
-        return MondrianResource.instance().MdxMemberName.str(getUniqueName());
-    }
+    return isChildOrEqualTo( this, uniqueName );
+  }
 
-    public abstract String getName();
-
-    public String getUniqueName() {
-        return uniqueName;
-    }
-
-    public String getCaption() {
-        // if there is a member formatter for the members level,
-        //  we will call this interface to provide the display string
-        MemberFormatter mf = getLevel().getMemberFormatter();
-        if (mf != null) {
-            return mf.formatMember(this);
-        }
-
-        // fallback if formatter is null
-        final String caption = super.getCaption();
-        return (caption != null)
-            ? caption
-            : getName();
-    }
-
-    public String getParentUniqueName() {
-        return parentMember == null
-            ? null
-            : parentMember.getUniqueName();
-    }
-
-    public Dimension getDimension() {
-        return level.getDimension();
-    }
-
-    public Hierarchy getHierarchy() {
-        return level.getHierarchy();
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public MemberType getMemberType() {
-        return MEMBER_TYPE_VALUES[flags & FLAG_TYPE_MASK];
-    }
-
-    public String getDescription() {
-        return (String) getPropertyValue(Property.DESCRIPTION.name);
-    }
-
-    public boolean isMeasure() {
-        return (flags & FLAG_MEASURE) != 0;
-    }
-
-    public boolean isAll() {
-        return (flags & FLAG_ALL) != 0;
-    }
-
-    public boolean isNull() {
-        return (flags & FLAG_NULL) != 0;
-    }
-
-    public boolean isCalculated() {
-        return (flags & FLAG_CALCULATED) != 0;
-    }
-
-    public boolean isEvaluated() {
-        // should just call isCalculated(), but called in tight loops
-        // and too many subclass implementations for jit to inline properly?
-        return (flags & FLAG_CALCULATED) != 0;
-    }
-
-    public OlapElement lookupChild(
-        SchemaReader schemaReader,
-        Id.Segment childName,
-        MatchType matchType)
-    {
-        return schemaReader.lookupMemberChildByName(
-            this, childName, matchType);
-    }
-
-    // implement Member
-    public Member getParentMember() {
-        return parentMember;
-    }
-
-    // implement Member
-    public boolean isChildOrEqualTo(Member member) {
-        // REVIEW: Using uniqueName to calculate ancestry seems inefficient,
-        //   because we can't afford to store every member's unique name, so
-        //   we want to compute it on the fly
-        assert !Bug.BugSegregateRolapCubeMemberFixed;
-        return (member != null) && isChildOrEqualTo(member.getUniqueName());
-    }
-
-   /**
-    * Returns whether this <code>Member</code>'s unique name is equal to, a
-    * child of, or a descendent of a member whose unique name is
-    * <code>uniqueName</code>.
-    */
-    public boolean isChildOrEqualTo(String uniqueName) {
-        if (uniqueName == null) {
-            return false;
-        }
-
-        return isChildOrEqualTo(this, uniqueName);
-    }
-
-    private static boolean isChildOrEqualTo(Member member, String uniqueName) {
-        while (true) {
-            String thisUniqueName = member.getUniqueName();
-            if (thisUniqueName.equals(uniqueName)) {
-                // found a match
-                return true;
-            }
-            // try candidate's parentMember
-            member = member.getParentMember();
-            if (member == null) {
-                // have reached root
-                return false;
-            }
-        }
-    }
-
-    /**
-     * Computes the value to be returned by {@link #isCalculated()}, so it can
-     * be cached in a variable.
-     *
-     * @param memberType Member type
-     * @return Whether this member is calculated
-     */
-    protected boolean computeCalculated(final MemberType memberType) {
-        // If the member is not created from the "with member ..." MDX, the
-        // calculated will be null. But it may be still a calculated measure
-        // stored in the cube.
-        return isCalculatedInQuery() || memberType == MemberType.FORMULA;
-    }
-
-    public int getSolveOrder() {
-        return -1;
-    }
-
-    /**
-     * Returns the expression by which this member is calculated. The expression
-     * is not null if and only if the member is not calculated.
-     *
-     * @post (return != null) == (isCalculated())
-     */
-    public Exp getExpression() {
-        return null;
-    }
-
-    // implement Member
-    public List<Member> getAncestorMembers() {
-        final SchemaReader schemaReader =
-            getDimension().getSchema().getSchemaReader();
-        final ArrayList<Member> ancestorList = new ArrayList<Member>();
-        schemaReader.getMemberAncestors(this, ancestorList);
-        return ancestorList;
-    }
-
-    /**
-     * Returns the ordinal of this member within its hierarchy.
-     * The default implementation returns -1.
-     */
-    public int getOrdinal() {
-        return -1;
-    }
-
-    /**
-     * Returns the order key of this member among its siblings.
-     * The default implementation returns null.
-     */
-    public Comparable getOrderKey() {
-        return null;
-    }
-
-    public boolean isHidden() {
+  private static boolean isChildOrEqualTo( Member member, String uniqueName ) {
+    while ( true ) {
+      String thisUniqueName = member.getUniqueName();
+      if ( thisUniqueName.equals( uniqueName ) ) {
+        // found a match
+        return true;
+      }
+      // try candidate's parentMember
+      member = member.getParentMember();
+      if ( member == null ) {
+        // have reached root
         return false;
+      }
     }
+  }
 
-    public Member getDataMember() {
-        return null;
-    }
+  /**
+   * Computes the value to be returned by {@link #isCalculated()}, so it can be cached in a variable.
+   *
+   * @param memberType Member type
+   * @return Whether this member is calculated
+   */
+  protected boolean computeCalculated( final MemberType memberType ) {
+    // If the member is not created from the "with member ..." MDX, the
+    // calculated will be null. But it may be still a calculated measure
+    // stored in the cube.
+    return isCalculatedInQuery() || memberType == MemberType.FORMULA;
+  }
 
-    public String getPropertyFormattedValue(String propertyName) {
-        return getPropertyValue(propertyName).toString();
-    }
+  public int getSolveOrder() {
+    return -1;
+  }
 
-    public boolean isParentChildPhysicalMember() {
-        return false;
-    }
+  /**
+   * Returns the expression by which this member is calculated. The expression is not null if and only if the
+   * member is
+   * not calculated.
+   *
+   * @post (return ! = null) == (isCalculated())
+   */
+  public Exp getExpression() {
+    return null;
+  }
 
-    public boolean isParentChildLeaf() {
-        return false;
-    }
+  // implement Member
+  public List<Member> getAncestorMembers() {
+    final SchemaReader schemaReader =
+      getDimension().getSchema().getSchemaReader();
+    final ArrayList<Member> ancestorList = new ArrayList<Member>();
+    schemaReader.getMemberAncestors( this, ancestorList );
+    return ancestorList;
+  }
+
+  /**
+   * Returns the ordinal of this member within its hierarchy. The default implementation returns -1.
+   */
+  public int getOrdinal() {
+    return -1;
+  }
+
+  /**
+   * Returns the order key of this member among its siblings. The default implementation returns null.
+   */
+  public Comparable getOrderKey() {
+    return null;
+  }
+
+  public boolean isHidden() {
+    return false;
+  }
+
+  public Member getDataMember() {
+    return null;
+  }
+
+  public String getPropertyFormattedValue( String propertyName ) {
+    return getPropertyValue( propertyName ).toString();
+  }
+
+  public boolean isParentChildPhysicalMember() {
+    return false;
+  }
+
+  public boolean isParentChildLeaf() {
+    return false;
+  }
+
+  public boolean isOnSameHierarchyChain( Member otherMember ) {
+    return ( (MemberBase) otherMember ).isOnSameHierarchyChainInternal( this );
+  }
+
+  public boolean isOnSameHierarchyChainInternal( MemberBase otherMember ) {
+    return FunUtil.isAncestorOf( otherMember, this, false ) || FunUtil.isAncestorOf( this, otherMember, false );
+  }
 }
 
 // End MemberBase.java
